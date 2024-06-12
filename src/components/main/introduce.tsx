8518c51dc7b6f978c9bf0827b86698787a788f46
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-12 23:38:37 +0900

    This project is Nexus

--- src/components/main/introduce.tsx
+++ src/components/main/introduce.tsx
@@ -1,9 +1,9 @@
 export default function Introduce() {
   return (
     <div className="border-b border-black h-[80%] flex flex-col gap-4 py-8">
-      <h1 className="font-bold p-4 text-center text-[4rem]">Cloud Base</h1>
+      <h1 className="font-bold p-4 text-center text-[4rem]">Nexus</h1>
       <h1 className="font-bold p-4 text-center text-[2rem]">Be a Contributor, Collaborator, Cofounder</h1>
-      <h2 className="font-bold p-4 text-center text-[1.5rem]">Cloud Base는 개발자들을 위한 기여, 협업, 공유 플랫폼입니다.</h2>
+      <h2 className="font-bold p-4 text-center text-[1.5rem]">Nexus는 교내 프로젝트 관리 & 지식공유 플랫폼입니다.</h2>
     </div>
   );
 }
