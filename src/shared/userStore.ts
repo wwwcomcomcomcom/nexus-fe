9c0c38533c10d7a30cf5a878d9abe08fd243c89b
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-25 14:50:37 +0900

    Feat - user zustand persist

--- src/shared/userStore.ts
+++ src/shared/userStore.ts
@@ -1,27 +1,22 @@
 import {create} from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface UserStore {
   accessToken: string|undefined;
   setAccessToken: (token: string) => void;
   isLogin: () => boolean;
-  loadfromSession: () => void;
-  saveToSession: () => void;
 }
 
-export const useUserStore = create<UserStore>((set,state) => ({
-  accessToken: undefined,
-  isLogin:()=>state().accessToken !== undefined,
-  setAccessToken: (token: string) => {
-    set({accessToken: token});
-    state().saveToSession();
-  },
-  loadfromSession:()=>{
-    const token = window.sessionStorage.getItem("accessToken");
-    if(token){
-      set({accessToken: token});
+export const useUserStore = create(
+  persist<UserStore>(
+    (set,get) => ({
+      accessToken: undefined,
+      setAccessToken: (token) => set({accessToken: token}),
+      isLogin: () => get().accessToken !== undefined,
+    }),
+    {
+      name: "user-storage",
+      storage: createJSONStorage(()=>sessionStorage)
     }
-  },
-  saveToSession:()=>{
-    window.sessionStorage.setItem("accessToken",state().accessToken!);
-  }
-}));
+  )
+);
