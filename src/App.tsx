5da186ebdd873dce176317472634f0a9935abee7
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-25 20:14:54 +0900

    Fix - correct import file name

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Layout from "./components/Layout.tsx";
 import HomeMain from "./components/main/HomePage.tsx";
-import Login from "./components/login/login.tsx";
+import Login from "./components/login/Login.tsx";
 import ProjectPage from "./components/main/ProjectPage.tsx";
 import ProjectList from './components/content/ProjectList.tsx';
 import ProfilePage from './components/main/ProfilePage.tsx';
