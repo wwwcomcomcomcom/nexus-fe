374e45f706a7af6a635491dc61f720d2e9ca021e
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-25 18:24:35 +0900

    Feat - profile btn and login btn switching by login

--- src/components/headBar/loginButton.tsx
+++ src/components/headBar/loginButton.tsx
@@ -1,5 +1,11 @@
-import {ButtonProps, DivProps} from "../../utils/typedef.ts";
+import { useNavigate } from "react-router-dom";
 
-export default function LoginButton(props:DivProps){
-  return <button {...(props as ButtonProps)}>Login</button>
+export default function LoginButton(){
+  const navigate = useNavigate();
+  return <button
+    onClick={()=>navigate('/login')}
+    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  >
+    Login
+  </button>;
 }
--- src/components/headBar/profileButton.tsx
+++ src/components/headBar/profileButton.tsx
@@ -1,10 +1,26 @@
 // import {Props} from "../../utils/typedef.ts";
 
-import {IoPersonOutline} from "react-icons/io5";
-import {IconBaseProps} from "react-icons";
-
-export default function ProfileButton(props:IconBaseProps){
+import { useEffect, useState } from "react";
+import { findUserData, useTokenStore, useUserStore } from "../../shared/userStore"
 
 
-  return <IoPersonOutline size={"24"} {...props}/>
+export default function ProfileButton(){
+  const accessToken = useTokenStore((s) => s.accessToken);
+  const user = useUserStore((s) => s.user);
+  const [img,setImg] = useState("");
+  console.log("hello");
+  useEffect(()=>{
+    findUserData(user,accessToken!).then((data) => {
+      setImg(data.avatar_url as string);
+    });
+  },[user,accessToken]);
+
+  return <div
+    className={`w-8 h-8 rounded-full bg-gray-400`}
+    style={{
+      backgroundImage: `url('${img}')`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    }}
+  />
 }
