8fe78ca53048b2f8eebc17f10a9c9c985654ff5f
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-25 15:03:01 +0900

    Feat - UserStore for user data

--- src/shared/userStore.ts
+++ src/shared/userStore.ts
@@ -1,5 +1,6 @@
 import {create} from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
+import { getUserData } from "./api";
 
 interface TokenStore {
   accessToken: string|undefined;
@@ -20,3 +21,19 @@ export const useTokenStore = create(
     }
   )
 );
+
+interface UserStore {
+  user:{[keyof:string]:unknown};
+  setUser: (user: {[keyof:string]:unknown}) => void;
+}
+export const useUserStore = create<UserStore>((set) => ({
+  user: {},
+  setUser: (user) => set({user}),
+}));
+
+export async function findUserData(user: {[key:string]:unknown},token: string):Promise<{[key:string]:unknown}> {
+  if(user.login !== undefined){
+    return Promise.resolve(user);
+  }
+  return await getUserData(token);
+}
