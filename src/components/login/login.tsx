930ed2c6c970e1ffc93fe26f2966c1a373dbdd9f
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-26 20:01:17 +0900

    Feat - gauth login button publishing

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -3,71 +3,101 @@ import { OauthClientId, getAccessToken } from "../../shared/api";
 import { useTokenStore } from "../../shared/userStore";
 import { useEffect } from "react";
 
-
 export default function Login() {
   const [query] = useSearchParams();
   const navigate = useNavigate();
-  const code = query.get('code');
-  const {setAccessToken} = useTokenStore((state) => state);
-  
+  const code = query.get("code");
+  const { setAccessToken } = useTokenStore((state) => state);
+
   useEffect(() => {
-    if(code){
+    if (code) {
       getAccessToken(code).then((accessToken) => {
         setAccessToken(accessToken);
         navigate("/");
       });
     }
-  },[code,setAccessToken,navigate]);
-  return <>
-    <a
-      className="inline-flex items-center justify-center rounded-full bg-gray-100 p-3 m-2 absolute"
-      href=".."
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="h-5 w-5"
+  }, [code, setAccessToken, navigate]);
+  return (
+    <>
+      <a
+        className="inline-flex items-center justify-center rounded-full bg-gray-100 p-3 m-2 absolute"
+        href=".."
       >
-        <path d="m12 19-7-7 7-7"></path>
-        <path d="M19 12H5"></path>
-      </svg>
-    </a>
-    <div className="mx-auto max-w-md space-y-6 py-12">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold">Sign in</h1>
-        <p className="text-gray-500">Sign in with your github account</p>
-      </div>
-      <div data-orientation="horizontal" role="none" className="shrink-0 bg-gray-100 h-[1px] w-full my-6"></div>
-      <img src="/github-icon.png" alt="github" className="mx-auto w-64" />
-      <a className="space-y-4 block" href={`https://github.com/login/oauth/authorize?client_id=${OauthClientId}`}>
-        <button
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium border transition-colors duration-300 active:bg-black active:text-white h-10 px-4 py-2 w-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-4 w-4"
-          >
-            <path
-              d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
-            <path d="M9 18c-4.51 2-5-2-7-2"></path>
-          </svg>
-          Sign in with GitHub
-        </button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="h-5 w-5"
+        >
+          <path d="m12 19-7-7 7-7"></path>
+          <path d="M19 12H5"></path>
+        </svg>
       </a>
-    </div>
-  </>;
+      <div className="mx-auto max-w-md space-y-6 py-12">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold">Sign in</h1>
+          <p className="text-gray-500">Sign in with your github account</p>
+        </div>
+        <div
+          data-orientation="horizontal"
+          role="none"
+          className="shrink-0 bg-gray-100 h-[1px] w-full my-6"
+        ></div>
+        <img src="/github-icon.png" alt="github" className="mx-auto w-64" />
+        <a
+          className="space-y-4 block"
+          href={`https://github.com/login/oauth/authorize?client_id=${OauthClientId}`}
+        >
+          <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium border transition-colors duration-300 hover:bg-black hover:text-white active:bg-gray-700 active:text-gray-300 h-10 px-4 py-2 w-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-4 w-4"
+            >
+              <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
+              <path d="M9 18c-4.51 2-5-2-7-2"></path>
+            </svg>
+            Sign in with GitHub
+          </button>
+        </a>
+
+        <a
+          className="space-y-4 block"
+          href={`https://github.com/login/oauth/authorize?client_id=${OauthClientId}`}
+        >
+          <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium border transition-colors duration-300 text-gauth-primary hover:bg-gauth-primary hover:text-white h-10 px-4 py-2 w-full">
+            <svg
+              width="16"
+              height="18"
+              viewBox="0 0 16 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="mr-2 h-4 w-4"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                fill="currentColor"
+                d="M0.197998 4.33382L7.901 0V5.31397L4.92057 6.9908L0.197998 4.33382ZM4.72255 7.31768L0 4.66071V13.3393L4.72255 10.6823V7.31768ZM5.30681 10.3536V7.64639L7.71274 9L5.30681 10.3536ZM4.9205 11.0092L0.197998 13.6662L7.901 18V12.6861L4.9205 11.0092ZM7.901 12.0287L5.50476 10.6805L7.901 9.33236V12.0287ZM8.29051 12.6861V18L16 13.6625L11.2775 11.0056L8.29051 12.6861ZM10.6933 10.6769L8.29051 12.0287V9.32506L10.6933 10.6769ZM11.2775 6.99444L16 4.33747L8.29051 0V5.31394L11.2775 6.99444ZM7.901 5.97139V8.66764L5.50483 7.31951L7.901 5.97139Z"
+              />
+            </svg>
+            Sign in with Gauth
+          </button>
+        </a>
+      </div>
+    </>
+  );
 }
