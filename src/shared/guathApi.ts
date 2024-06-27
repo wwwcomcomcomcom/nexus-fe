b99439f01b8ec7e9433518072dfb4bb840504b0e
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-27 09:14:36 +0900

    Feat - signup with gauth

--- src/shared/guathApi.ts
+++ src/shared/guathApi.ts
@@ -2,8 +2,18 @@ import axios from "axios";
 
 export const GauthOauthClientId = "872c10e9c0d74da18917049d033aa68e4a0edc5aeb0d4b7f97e28fe4a12da3c0";
 const ApiBaseUrl = "http://localhost:5173";
-export async function getAccessToken(code: string):Promise<string> {
-  return await axios.get(`${ApiBaseUrl}/api/gauth/token?accessCode=${code}`).then((res) => {
+
+export async function signup(accessCode: string):Promise<string> {
+  return await axios.post(`${ApiBaseUrl}/api/user/signup?accessCode=${accessCode}`).then((res) => {
+    if(res.status === 200 && typeof res.data === 'string'){
+      return res.data;
+    }
+    throw new Error("Invalid response");
+  });
+}
+
+export async function login(accessCode: string):Promise<string> {
+  return await axios.get(`${ApiBaseUrl}/api/gauth/token?accessCode=${accessCode}`).then((res) => {
     if(res.status === 200 && typeof res.data === 'string'){
       return res.data;
     }
