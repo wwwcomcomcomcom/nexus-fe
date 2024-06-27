9330ecf264291d5daf783df9aa3508275cd32496
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-27 09:44:03 +0900

    Feat - login with github

--- src/shared/githubApi.ts
+++ src/shared/githubApi.ts
@@ -5,21 +5,11 @@ export const GithubOauthClientId = "Ov23liUtVGUymJiEuINj";
 const ApiBaseUrl = "http://localhost:5173";
 
 
-export async function getAccessToken(code: string):Promise<string> {
-  return await axios.get(`${ApiBaseUrl}/api/oauth/login?accessCode=${code}`).then((res) => {
+export async function login(accessCode: string):Promise<string> {
+  return await axios.get(`${ApiBaseUrl}/api/user/login/github?accessCode=${accessCode}`).then((res) => {
     if(res.status === 200 && typeof res.data === 'string'){
       return res.data;
     }
     throw new Error("Invalid response");
-  });
-}
-export async function getUserData(accessToken: string):Promise<{[key:string]:unknown}> {
-  return await axios.get(`https://api.github.com/user`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      "X-GitHub-Api-Version": "2022-11-28"
-    },
-  }).then((res) => {
-    return res.data as {[key:string]:unknown};
   });
 }
