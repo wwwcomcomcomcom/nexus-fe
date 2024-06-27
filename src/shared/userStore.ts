1ba2f258bd42fe6cdfb223b488765508431def0f
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-27 11:27:52 +0900

    Remove - tokenStore

--- src/shared/userStore.ts
+++ src/shared/userStore.ts
@@ -1,27 +1,4 @@
 import {create} from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
-import { getUserData } from "./githubApi";
-
-interface TokenStore {
-  accessToken: string|undefined;
-  setAccessToken: (token: string) => void;
-  isLogin: () => boolean;
-}
-
-export const useTokenStore = create(
-  persist<TokenStore>(
-    (set,get) => ({
-      accessToken: undefined,
-      setAccessToken: (token) => set({accessToken: token}),
-      isLogin: () => get().accessToken !== undefined,
-    }),
-    {
-      name: "user-storage",
-      storage: createJSONStorage(()=>sessionStorage)
-    }
-  )
-);
-
 interface UserStore {
   user:{[keyof:string]:unknown};
   setUser: (user: {[keyof:string]:unknown}) => void;
@@ -30,10 +7,3 @@ export const useUserStore = create<UserStore>((set) => ({
   user: {},
   setUser: (user) => set({user}),
 }));
-
-export async function findUserData(user: {[key:string]:unknown},token: string):Promise<{[key:string]:unknown}> {
-  if(user.login !== undefined){
-    return Promise.resolve(user);
-  }
-  return await getUserData(token);
-}
