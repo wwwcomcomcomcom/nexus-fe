c6785e7140a4ad2f3294c64859f95dc15f2d9c6a
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-02 16:51:01 +0900

    Feat - publish profile page like github

--- src/components/main/ProfilePage.tsx
+++ src/components/main/ProfilePage.tsx
@@ -1,19 +1,39 @@
 import { useUpdatedUserStore } from "../../shared/api";
+import { getAllProjectEntity } from "../../test/TestVariables";
+import ProjectCard from "../card/ProjectCard";
 
 export default function ProfilePage() {
-  const {isLogin,user} = useUpdatedUserStore();
-  if(isLogin() === false) return <h1>You are not logged in</h1>;
-  return <div className="flex flex-col items-center space-y-4">
-    <div className="flex items-center space-x-4">
-      <img
-        className="w-24 h-24 rounded-full"
-        src={user.profileImageUrl as string}
-        alt="profile"
-      />
-      <div>
-        <h1 className="text-2xl font-bold">{user.login as string}</h1>
-        <p className="text-gray-500"></p>
-      </div>
+  const { isLogin, user } = useUpdatedUserStore();
+  if (isLogin() === false) return <h1>You are not logged in</h1>;
+  return (
+    <div className="flex justify-center pt-4">
+      <aside className="flex flex-col items-center space-x-4 flex-grow basis-0">
+        <img
+          className="w-52 h-52 rounded-full border-2 border-gray-300"
+          src={user.profileImageUrl as string}
+          alt="profile"
+        />
+        <div>
+          <h1 className="text-2xl font-bold">{user.name as string}</h1>
+          <p className="text-gray-500"></p>
+        </div>
+      </aside>
+      <main className="grow-[2] basis-0 flex flex-col gap-4 px-10">
+        <div>
+          <h1 className="text-2xl font-bold">My Projects</h1>
+          <div className="grid grid-cols-2 gap-4 h-[17rem] overflow-y-scroll scrollbar-hide mb-4">
+            {getAllProjectEntity(10).map((project) => (
+              <ProjectCard key={project.id} project={project} className="h-[8rem]"/>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold">My Posts</h1>
+          <div className="grid grid-cols-2 gap-4 h-[17rem] overflow-y-scroll scrollbar-hide mb-4">
+            <h1>TODO:Impl post card</h1>
+          </div>
+        </div>
+      </main>
     </div>
-  </div>
+  );
 }
