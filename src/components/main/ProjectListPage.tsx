28176b555449ea3dfbe362396c5bbe393423ae0e
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-02 17:54:17 +0900

    Feat - navigate button to create project

--- src/components/main/ProjectListPage.tsx
+++ src/components/main/ProjectListPage.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import ProjectList from "../content/ProjectList";
 
 export default function ProjectListPage() {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col w-full h-full mt-2">
       <div className="flex flex-row w-full px-10 place-content-between">
@@ -9,7 +11,7 @@ export default function ProjectListPage() {
           <p className="text-lg text-gray-500">List of all projects</p>
         </div>
         <div className="flex flex-row items-center">
-          <button className="bg-blue-400 rounded-md h-10 px-3">Create Project</button>
+          <button className="bg-blue-400 rounded-md h-10 px-3" onClick={()=>navigate("/createProject")}>Create Project</button>
         </div>
       </div>
       <ProjectList />
