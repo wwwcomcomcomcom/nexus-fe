226da5e82b495638e43f408fe6910f1bf9835a4c
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-02 17:53:00 +0900

    Feat - use Icon Components

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -2,6 +2,7 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import * as GithubApi from "../../shared/githubApi";
 import { useEffect } from "react";
 import * as GauthApi from "../../shared/guathApi";
+import GauthIcon from "../icons/GauthIcon";
 
 export default function Login() {
   const [query] = useSearchParams();
@@ -87,21 +88,7 @@ export default function Login() {
           href={`https://gauth.co.kr/login?client_id=${GauthApi.GauthOauthClientId}&redirect_uri=http://localhost:5173/login?gauth`}
         >
           <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium border transition-colors duration-300 text-gauth-primary hover:bg-gauth-primary hover:text-white h-10 px-4 py-2 w-full">
-            <svg
-              width="16"
-              height="18"
-              viewBox="0 0 16 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="mr-2 h-4 w-4"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                fill="currentColor"
-                d="M0.197998 4.33382L7.901 0V5.31397L4.92057 6.9908L0.197998 4.33382ZM4.72255 7.31768L0 4.66071V13.3393L4.72255 10.6823V7.31768ZM5.30681 10.3536V7.64639L7.71274 9L5.30681 10.3536ZM4.9205 11.0092L0.197998 13.6662L7.901 18V12.6861L4.9205 11.0092ZM7.901 12.0287L5.50476 10.6805L7.901 9.33236V12.0287ZM8.29051 12.6861V18L16 13.6625L11.2775 11.0056L8.29051 12.6861ZM10.6933 10.6769L8.29051 12.0287V9.32506L10.6933 10.6769ZM11.2775 6.99444L16 4.33747L8.29051 0V5.31394L11.2775 6.99444ZM7.901 5.97139V8.66764L5.50483 7.31951L7.901 5.97139Z"
-              />
-            </svg>
+            <GauthIcon className="h-4 w-4 mr-2" />
             Sign in with Gauth
           </button>
         </a>
--- src/components/main/introduce.tsx
+++ src/components/main/introduce.tsx
@@ -3,6 +3,7 @@ import ProjectCard from "../card/ProjectCard";
 import { generateProjectEntity } from "../../test/TestVariables";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import GauthIcon from "../icons/GauthIcon";
 
 interface ScrollPageProps extends HTMLMotionProps<"div">{
   childProps?:HTMLMotionProps<"div">
@@ -49,22 +50,8 @@ export default function Introduce() {
       <ScrollPage childProps={{className:"flex flex-col items-center gap-5"}}>
         <h1 className="font-bold p-4 text-center text-[2rem]">Nexus와 함께 프로젝트를 시작해보세요!</h1>
         <button onClick={()=>navigate("/login")} className="p-4 rounded-lg bg-gauth-primary text-white font-bold hover:bg-blue-400 hover:shadow hover:shadow-blue-700">
-          <svg
-              width="16"
-              height="18"
-              viewBox="0 0 16 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="inline mr-2 -translate-y-0.5"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                fill="currentColor"
-                d="M0.197998 4.33382L7.901 0V5.31397L4.92057 6.9908L0.197998 4.33382ZM4.72255 7.31768L0 4.66071V13.3393L4.72255 10.6823V7.31768ZM5.30681 10.3536V7.64639L7.71274 9L5.30681 10.3536ZM4.9205 11.0092L0.197998 13.6662L7.901 18V12.6861L4.9205 11.0092ZM7.901 12.0287L5.50476 10.6805L7.901 9.33236V12.0287ZM8.29051 12.6861V18L16 13.6625L11.2775 11.0056L8.29051 12.6861ZM10.6933 10.6769L8.29051 12.0287V9.32506L10.6933 10.6769ZM11.2775 6.99444L16 4.33747L8.29051 0V5.31394L11.2775 6.99444ZM7.901 5.97139V8.66764L5.50483 7.31951L7.901 5.97139Z"
-              />
-            </svg>
-            Gauth로 시작하기
+          <GauthIcon className="inline mr-2 -translate-y-0.5"/>
+          Gauth로 시작하기
         </button>
       </ScrollPage>
     </motion.div>
