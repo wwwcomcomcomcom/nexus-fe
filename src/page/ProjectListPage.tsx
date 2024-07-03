392d3e6be429f99bfb3abaaead42ccec6cb6acef
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-03 20:07:15 +0900

    Feat - hide create post button for guest

--- src/page/ProjectListPage.tsx
+++ src/page/ProjectListPage.tsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import ProjectList from "../component/ProjectList.tsx";
+import { useUpdatedUserStore } from "../shared/api.ts";
 
 export default function ProjectListPage() {
   const navigate = useNavigate();
+  const userStore = useUpdatedUserStore();
   return (
     <div className="flex flex-col w-full h-full mt-2">
       <div className="flex flex-row w-full px-10 place-content-between">
@@ -10,9 +12,12 @@ export default function ProjectListPage() {
           <h1 className="text-4xl font-bold">Projects</h1>
           <p className="text-lg text-gray-500">List of all projects</p>
         </div>
-        <div className="flex flex-row items-center">
+        {
+          userStore.isLogin() && <div className="flex flex-row items-center">
           <button className="bg-blue-400 rounded-md h-10 px-3" onClick={()=>navigate("/createProject")}>Create Project</button>
         </div>
+        }
+        
       </div>
       <ProjectList />
     </div>
