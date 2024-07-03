41013f86aa84d980b5ce93cce0c47599740cabd8
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-03 21:17:43 +0900

    Feat - submit project data param

--- src/component/createProject/createProjectApi.ts
+++ src/component/createProject/createProjectApi.ts
@@ -1,1 +1,9 @@
+import axios from "axios";
+import { CreateProjectDto } from "./projectFormStore";
+
 //TODO:impl createProjectApi
+//TODO:move createProjectDto from projectFormStore
+export function submitProjectData(projectData: CreateProjectDto) {
+  //axios
+  return null;
+}
