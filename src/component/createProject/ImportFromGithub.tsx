ee283763ce9e5ded623c443f60eb6b91aeb1df25
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-03 21:11:16 +0900

    Update - rename CreateFromGithub to ImportFromGithub

--- src/component/createProject/ImportFromGithub.tsx
+++ src/component/createProject/ImportFromGithub.tsx
@@ -1,4 +1,4 @@
-export default function CreateFromGithub({setViewPage}:{setViewPage:(page:number)=>void}) {
+export default function ImportFromGithub({setViewPage}:{setViewPage:(page:number)=>void}) {
   
   return <>
     <div
--- src/page/CreateProjectPage.tsx
+++ src/page/CreateProjectPage.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react"
 import CreateNewProject from "../component/createProject/CreateNewProject.tsx";
 import SelectCreateMode from "../component/createProject/SelectCreateMode.tsx";
-import CreateFromGithub from "../component/createProject/CreateFromGithub.tsx";
+import ImportFromGithub from "../component/createProject/ImportFromGithub.tsx";
 
 export default function CreateProjectPage() {
   const [viewPage,setViewPage] = useState(0);
   const pages:JSX.Element[] = [
     <CreateNewProject setViewPage={setViewPage}/>,
     <SelectCreateMode setViewPage={setViewPage}/>,
-    <CreateFromGithub setViewPage={setViewPage}/>
+    <ImportFromGithub setViewPage={setViewPage}/>
   ]
   return <main className="flex justify-center h-fit">
     {pages[viewPage]}
