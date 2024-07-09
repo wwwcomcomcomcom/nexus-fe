f6b4d1864eed2c14b727e7f386e80e7801f9aa5c
hyojoo <jhj080802@naver.com>
2024-07-09 09:00:53 +0900

    change border color

--- src/component/BestProjectList.tsx
+++ src/component/BestProjectList.tsx
@@ -6,46 +6,53 @@ import "@egjs/react-flicking/dist/flicking.css";
 import { AutoPlay } from "@egjs/flicking-plugins";
 
 interface BestProjectListProps {
-  projects: ProjectEntity[];
+    projects: ProjectEntity[];
 }
 
 export const BestProjectList: React.FC<BestProjectListProps> = ({
-  projects,
+    projects,
 }) => {
-  const ref = useRef(null);
-  const autoPlay = new AutoPlay({
-    duration: 1500,
-    direction: "NEXT",
-    stopOnHover: true,
-  });
-
-  function updateSlider(e: MoveEvent<Flicking> | ReadyEvent<Flicking>) {
-    e.currentTarget.panels.forEach((panel) => {
-      if(Math.abs(panel.progress) < 0.2) {
-        panel.element.style.transform = `scale(${1-Math.abs(panel.progress) + 0.2})`;
-      }else{
-        panel.element.style.transform = "scale(1)";
-      }
+    const ref = useRef(null);
+    const autoPlay = new AutoPlay({
+        duration: 1500,
+        direction: "NEXT",
+        stopOnHover: true,
     });
-  }
 
-  return (
-    <div className="border-b border-black">
-      <h2 className="text-2xl font-bold p-4 text-center">Best Projects</h2>
-      <Flicking
-        className="best-project-list"
-        circular={true}
-        onReady={updateSlider}
-        onMove={updateSlider}
-        ref={ref}
-        plugins={[autoPlay]}
-      >
-        {projects.map((project) => (
-          <div key={project.id} className="my-8 mx-8 w-[80%] sm:w-[70%] sm:mx-8 md:w-[40%] md:mx-6 lg:w-[25%] lg:mx-6">
-            <ProjectCard project={project} />
-          </div>
-        ))}
-      </Flicking>
-    </div>
-  );
+    function updateSlider(e: MoveEvent<Flicking> | ReadyEvent<Flicking>) {
+        e.currentTarget.panels.forEach((panel) => {
+            if (Math.abs(panel.progress) < 0.2) {
+                panel.element.style.transform = `scale(${
+                    1 - Math.abs(panel.progress) + 0.2
+                })`;
+            } else {
+                panel.element.style.transform = "scale(1)";
+            }
+        });
+    }
+
+    return (
+        <div className="border-b border-gray-300">
+            <h2 className="text-2xl font-bold p-4 text-center">
+                Best Projects
+            </h2>
+            <Flicking
+                className="best-project-list"
+                circular={true}
+                onReady={updateSlider}
+                onMove={updateSlider}
+                ref={ref}
+                plugins={[autoPlay]}
+            >
+                {projects.map((project) => (
+                    <div
+                        key={project.id}
+                        className="my-8 mx-8 w-[80%] sm:w-[70%] sm:mx-8 md:w-[40%] md:mx-6 lg:w-[25%] lg:mx-6"
+                    >
+                        <ProjectCard project={project} />
+                    </div>
+                ))}
+            </Flicking>
+        </div>
+    );
 };
--- src/component/headBar/Header.tsx
+++ src/component/headBar/Header.tsx
@@ -4,49 +4,55 @@ import LoginButton from "./LoginButton";
 import { useUpdatedUserStore } from "../../shared/api";
 
 export default function Header() {
-  const navigate = useNavigate();
-  const {isLogin} = useUpdatedUserStore();
+    const navigate = useNavigate();
+    const { isLogin } = useUpdatedUserStore();
 
-  return (
-    <header className="flex items-center justify-between px-4 py-2 border-b bg-white border-gray-100 sm:py-4 md:py-6 dark:border-gray-800 w-full sticky top-0 z-10 h-20">
-      <div className="flex items-center space-x-4">
-        <div className="rounded-lg bg-gray-100 w-10 h-10 dark:bg-gray-300 max-xxs:hidden"></div>
-        <nav className="flex items-center space-x-4">
-          <button className="font-semibold" onClick={() => navigate("/")}>
-            Home
-          </button>
-          <button className="font-semibold" onClick={() => navigate("/projects")}>
-            Projects
-          </button>
-        </nav>
-      </div>
-      <div className="flex items-center space-x-4">
-        <div className="relative w-60 max-sm:w-full max-xs:hidden">
-          <div className="absolute inset-y-0 flex items-center pl-3 pointer-events-none">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-5 w-5 text-gray-400"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <path d="m21 21-4.3-4.3"></path>
-            </svg>
-          </div>
-          <input
-            className="flex h-10 rounded-md border bg-background px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 pl-10 border-gray-200 text-sm dark:border-gray-800 w-60 max-sm:w-full"
-            placeholder="Search"
-            type="text"
-          />
-        </div>
-        {isLogin() ? <ProfileButton /> : <LoginButton />}
-      </div>
-    </header>
-  );
+    return (
+        <header className="flex items-center justify-between px-4 py-2 border-b bg-white border-gray-300 sm:py-4 md:py-6 w-full sticky top-0 z-10 h-20">
+            <div className="flex items-center space-x-4">
+                <div className="rounded-lg bg-gray-100 w-10 h-10 dark:bg-gray-300 max-xxs:hidden"></div>
+                <nav className="flex items-center space-x-4">
+                    <button
+                        className="font-semibold"
+                        onClick={() => navigate("/")}
+                    >
+                        Home
+                    </button>
+                    <button
+                        className="font-semibold"
+                        onClick={() => navigate("/projects")}
+                    >
+                        Projects
+                    </button>
+                </nav>
+            </div>
+            <div className="flex items-center space-x-4">
+                <div className="relative w-60 max-sm:w-full max-xs:hidden">
+                    <div className="absolute inset-y-0 flex items-center pl-3 pointer-events-none">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="h-5 w-5 text-gray-400"
+                        >
+                            <circle cx="11" cy="11" r="8"></circle>
+                            <path d="m21 21-4.3-4.3"></path>
+                        </svg>
+                    </div>
+                    <input
+                        className="flex h-10 rounded-md border bg-background px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 pl-10 border-gray-400 text-sm w-60 max-sm:w-full"
+                        placeholder="Search"
+                        type="text"
+                    />
+                </div>
+                {isLogin() ? <ProfileButton /> : <LoginButton />}
+            </div>
+        </header>
+    );
 }
