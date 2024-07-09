14f8f2cf253579e0e1f1022459a39564e33b9496
hyojoo <jhj080802@naver.com>
2024-07-09 09:06:43 +0900

    format

--- src/component/Introduce.tsx
+++ src/component/Introduce.tsx
@@ -1,9 +1,9 @@
 import {
-    useScroll,
-    motion,
-    useTransform,
-    HTMLMotionProps,
-    MotionValue,
+  useScroll,
+  motion,
+  useTransform,
+  HTMLMotionProps,
+  MotionValue,
 } from "framer-motion";
 import ProjectCard from "./projectCard/ProjectCard.tsx";
 import { generateProjectEntity } from "../shared/apiMockup.ts";
@@ -12,101 +12,97 @@ import { useNavigate } from "react-router-dom";
 import GauthIcon from "./icons/GauthIcon.tsx";
 
 interface ScrollPageProps extends HTMLMotionProps<"div"> {
-    childProps?: HTMLMotionProps<"div">;
+  childProps?: HTMLMotionProps<"div">;
 }
 
 function ScrollPage(props: ScrollPageProps) {
-    return (
-        <motion.div
-            className="h-full snap-center flex justify-center items-center"
-            {...props}
-        >
-            <motion.div {...props.childProps}>{props.children}</motion.div>
-        </motion.div>
-    );
+  return (
+    <motion.div
+      className="h-full snap-center flex justify-center items-center"
+      {...props}
+    >
+      <motion.div {...props.childProps}>{props.children}</motion.div>
+    </motion.div>
+  );
 }
 
 function useParallaxEffect(nthChild: number, progress: MotionValue<number>) {
-    return {
-        opacity: useTransform(
-            progress,
-            [(nthChild - 1) / 3, nthChild / 3],
-            [1, 0]
-        ),
-        offset: useTransform(
-            progress,
-            [(nthChild - 1) / 3, nthChild / 3],
-            [0, 250]
-        ),
-    };
+  return {
+    opacity: useTransform(progress, [(nthChild - 1) / 3, nthChild / 3], [1, 0]),
+    offset: useTransform(
+      progress,
+      [(nthChild - 1) / 3, nthChild / 3],
+      [0, 250]
+    ),
+  };
 }
 
 export default function Introduce() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const ref = useRef(null);
-    const { scrollYProgress } = useScroll({ container: ref });
-    const { opacity: opacity1, offset: offset1 } = useParallaxEffect(
-        1,
-        scrollYProgress
-    );
-    const { opacity: opacity2, offset: offset2 } = useParallaxEffect(
-        2,
-        scrollYProgress
-    );
-    const { opacity: opacity3, offset: offset3 } = useParallaxEffect(
-        3,
-        scrollYProgress
-    );
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ container: ref });
+  const { opacity: opacity1, offset: offset1 } = useParallaxEffect(
+    1,
+    scrollYProgress
+  );
+  const { opacity: opacity2, offset: offset2 } = useParallaxEffect(
+    2,
+    scrollYProgress
+  );
+  const { opacity: opacity3, offset: offset3 } = useParallaxEffect(
+    3,
+    scrollYProgress
+  );
 
-    return (
-        <motion.div
-            ref={ref}
-            className="border-b border-gray-300 h-[90vh] overflow-scroll snap-y snap-mandatory scrollbar-hide"
+  return (
+    <motion.div
+      ref={ref}
+      className="border-b border-gray-300 h-[90vh] overflow-scroll snap-y snap-mandatory scrollbar-hide"
+    >
+      <ScrollPage
+        style={{ opacity: opacity1 }}
+        childProps={{ style: { y: offset1 } }}
+      >
+        <h1 className="font-bold p-4 text-center text-[4rem]">Nexus</h1>
+        <h1 className="font-bold p-4 text-center text-[2rem]">
+          Nexus는 교내 프로젝트 관리 & 지식공유 플랫폼입니다.
+        </h1>
+      </ScrollPage>
+      <ScrollPage
+        style={{ opacity: opacity2 }}
+        childProps={{ style: { y: offset2 } }}
+      >
+        <h1 className="font-bold p-4 text-center text-[2rem]">
+          프로젝트 인원 모집 / 관리
+        </h1>
+        <ProjectCard project={generateProjectEntity()} />
+      </ScrollPage>
+      <ScrollPage
+        style={{ opacity: opacity3 }}
+        childProps={{ style: { y: offset3 } }}
+      >
+        <h1 className="font-bold p-4 text-center text-[2rem]">
+          개발 정보 공유 커뮤니티
+        </h1>
+        <h2 className="text-[1.5rem]">
+          AI 추천기반 최신 기술정보 | 자유로운 정보공유 게시판
+        </h2>
+      </ScrollPage>
+      <ScrollPage
+        childProps={{ className: "flex flex-col items-center gap-5" }}
+      >
+        <h1 className="font-bold p-4 text-center text-[2rem]">
+          Nexus와 함께 프로젝트를 시작해보세요!
+        </h1>
+        <button
+          onClick={() => navigate("/login")}
+          className="p-4 rounded-lg bg-gauth-primary text-white font-bold hover:bg-blue-400 hover:shadow hover:shadow-blue-700"
         >
-            <ScrollPage
-                style={{ opacity: opacity1 }}
-                childProps={{ style: { y: offset1 } }}
-            >
-                <h1 className="font-bold p-4 text-center text-[4rem]">Nexus</h1>
-                <h1 className="font-bold p-4 text-center text-[2rem]">
-                    Nexus는 교내 프로젝트 관리 & 지식공유 플랫폼입니다.
-                </h1>
-            </ScrollPage>
-            <ScrollPage
-                style={{ opacity: opacity2 }}
-                childProps={{ style: { y: offset2 } }}
-            >
-                <h1 className="font-bold p-4 text-center text-[2rem]">
-                    프로젝트 인원 모집 / 관리
-                </h1>
-                <ProjectCard project={generateProjectEntity()} />
-            </ScrollPage>
-            <ScrollPage
-                style={{ opacity: opacity3 }}
-                childProps={{ style: { y: offset3 } }}
-            >
-                <h1 className="font-bold p-4 text-center text-[2rem]">
-                    개발 정보 공유 커뮤니티
-                </h1>
-                <h2 className="text-[1.5rem]">
-                    AI 추천기반 최신 기술정보 | 자유로운 정보공유 게시판
-                </h2>
-            </ScrollPage>
-            <ScrollPage
-                childProps={{ className: "flex flex-col items-center gap-5" }}
-            >
-                <h1 className="font-bold p-4 text-center text-[2rem]">
-                    Nexus와 함께 프로젝트를 시작해보세요!
-                </h1>
-                <button
-                    onClick={() => navigate("/login")}
-                    className="p-4 rounded-lg bg-gauth-primary text-white font-bold hover:bg-blue-400 hover:shadow hover:shadow-blue-700"
-                >
-                    <GauthIcon className="inline mr-2 -translate-y-0.5" />
-                    Gauth로 시작하기
-                </button>
-            </ScrollPage>
-        </motion.div>
-    );
+          <GauthIcon className="inline mr-2 -translate-y-0.5" />
+          Gauth로 시작하기
+        </button>
+      </ScrollPage>
+    </motion.div>
+  );
 }
--- src/component/footer/footer.tsx
+++ src/component/footer/footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
-    return (
-        <footer className="flex items-center justify-center w-full p-4 border-t border-gray-300">
-            <p className="text-sm text-center text-gray-500 dark:text-gray-400">
-                © 2024 Cloud Base, All rights reserved.
-            </p>
-        </footer>
-    );
+  return (
+    <footer className="flex items-center justify-center w-full p-4 border-t border-gray-300">
+      <p className="text-sm text-center text-gray-500 dark:text-gray-400">
+        © 2024 Cloud Base, All rights reserved.
+      </p>
+    </footer>
+  );
 }
