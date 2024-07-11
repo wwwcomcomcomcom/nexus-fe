35256d19006aec57451c8d26542e712da6170e69
hyojoo <jhj080802@naver.com>
2024-07-11 17:00:48 +0900

    adjust location : center

--- src/component/ProjectList.tsx
+++ src/component/ProjectList.tsx
@@ -30,13 +30,16 @@ export default function ProjectList() {
   });
 
   return (
-    <main className="flex flex-1 overflow-y-auto max-h-[70vh] relative overflow-hidden" ref={scrollRef}>
+    <main
+      className="flex flex-1 overflow-y-auto max-h-[70vh] relative overflow-hidden"
+      ref={scrollRef}
+    >
       <div className="w-[350px] h-[350px] bg-[#DAE9FF] rounded-full absolute top-[10rem] translate-x-[45rem] z-0"></div>
       <div className="w-[400px] h-[400px] bg-[#DAE9FF] rounded-full absolute top-[50rem] -translate-x-[17rem] z-0"></div>
       {/* <div className="w-[200px] h-[200px] bg-[#DAE9FF] rounded-full absolute top-[95rem] translate-x-[67rem] z-10"></div> */}
       <div className="w-[650px] h-[650px] bg-[#e2ecfc] rounded-full absolute top-[90rem] translate-x-[85rem] z-0"></div>
-      <div className="container grid px-4 py-6 md:gap-8 md:px-6 w-[100%] justify-center ">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-8 justify-center">
+      <div className="grid px-4 py-6 md:gap-8 md:px-6 justify-center w-[100%]">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
           {projects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
