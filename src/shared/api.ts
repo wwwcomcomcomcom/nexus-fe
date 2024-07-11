faf8aba90a7a1300b3533a2c177a24cdab6e2f1f
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-12 07:59:52 +0900

    Feat - jwt api fetching & save in store

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -4,28 +4,40 @@ import { useEffect } from "react";
 
 export const ApiBaseUrl = "http://localhost:5173";
 
-export function useUpdatedUserStore(){
+export function useUpdatedUserStore() {
   const store = useUserStore();
-  useEffect(()=>{
-    axios.get(`${ApiBaseUrl}/api/user/info`).then((res)=>{
-      if(res.status === 200 && res.data.name){
-        store.setUser(res.data);
-      }
-    }).catch((e)=>{
-      console.log(e);
-    });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[]);
+  useEffect(() => {
+    if(!store.isLogin()){
+      store.resetStore();
+      return;
+    }
+    axios
+      .get(`${ApiBaseUrl}/api/user/info`, {
+        headers: { Authorization: `Bear ${store.jwt!}` },
+      })
+      .then((res) => {
+        if (res.status === 200 && res.data.name) {
+          store.setUser(res.data);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return store;
 }
 
-export function getUserInfo(){
-  axios.get(`${ApiBaseUrl}/api/user/info`).then((res) => {
-    if(res.status === 200 && res.data.name){
-      return res.data;
-    }
-    throw new Error(`Invalid response ${res.status}`);
-  }).catch((e) => {
-    throw new Error("Failed to get user info" + e.message);
-  });
-}
+// export function getUserInfo() {
+//   axios
+//     .get(`${ApiBaseUrl}/api/user/info`)
+//     .then((res) => {
+//       if (res.status === 200 && res.data.name) {
+//         return res.data;
+//       }
+//       throw new Error(`Invalid response ${res.status}`);
+//     })
+//     .catch((e) => {
+//       throw new Error("Failed to get user info" + e.message);
+//     });
+// }
--- src/shared/userStore.ts
+++ src/shared/userStore.ts
@@ -1,21 +1,47 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
 interface UserStore {
-  user:{[keyof:string]:unknown};
-  setUser: (user: {[keyof:string]:unknown}) => void;
+  user: { [keyof: string]: unknown };
+  jwt: string | undefined;
+  setUser: (user: { [keyof: string]: unknown }) => void;
+  setJwt: (jwt: string) => void;
+  hasUserData: () => boolean;
   isLogin: () => boolean;
+  resetStore: () => void;
 }
 export const useUserStore = create(
   persist<UserStore>(
-    (set,get) => ({
+    (set, get) => ({
       user: {},
-      isLogin: ()=> get().user.name !== undefined,
+      jwt: undefined,
+      hasUserData: () => get().user.name !== undefined,
+      setJwt: (jwt) => set({ jwt }),
       setUser: (user) => set({ user }),
+      isLogin: () =>
+        get().jwt !== undefined &&
+        (parseJwt(get().jwt!).exp as number) > Date.now(),
+      resetStore: () => set({ user: {}, jwt: undefined }),
     }),
     {
       name: "user-storage",
-      storage: createJSONStorage(()=>sessionStorage),
+      storage: createJSONStorage(() => sessionStorage),
     }
   )
 );
+
+function parseJwt(token: string): { [keyof: string]: unknown } {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
