ed2b9d58e791ca3f39cc9655f0d60a35b0e32d89
hyojoo <jhj080802@naver.com>
2024-07-12 08:04:06 +0900

    remove mt

--- src/page/PostListPage.tsx
+++ src/page/PostListPage.tsx
@@ -7,11 +7,14 @@ export default function PostsPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col w-full h-full mt-2">
+    <div className="flex flex-col w-full h-full ">
       <div className="flex flex-row w-full px-10 place-content-between justify-center ">
         {userStore.isLogin() && (
           <div className="flex flex-row items-center">
-            <button className="bg-blue-400 rounded-md h-10 px-3" onClick={() => navigate("/createPost")}>
+            <button
+              className="bg-blue-400 rounded-md h-10 px-3"
+              onClick={() => navigate("/createPost")}
+            >
               Create Post
             </button>
           </div>
