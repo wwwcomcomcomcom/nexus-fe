aade17381357a1664a2785c521d8e2dee5ffc2a7
hyojoo <jhj080802@naver.com>
2024-07-11 20:09:16 +0900

    change circle location and size

--- src/component/ProjectList.tsx
+++ src/component/ProjectList.tsx
@@ -34,18 +34,18 @@ export default function ProjectList() {
       className="flex flex-1 overflow-y-auto max-h-[70vh] relative overflow-hidden"
       ref={scrollRef}
     >
-      {/* <div className="w-[650px] h-[650px] bg-[#DAE9FF] rounded-full absolute top-[5rem] translate-x-[65rem] z-0"></div> */}
-      <div className="w-[650px] h-[650px] bg-[#e2ecfc] rounded-full absolute top-[5rem] translate-x-[85rem] z-0"></div>
-      <div className="w-[400px] h-[400px] bg-[#DAE9FF] rounded-full absolute top-[60rem] -translate-x-[17rem] z-0"></div>
-      <div className="w-[200px] h-[200px] bg-[#FFF7E3] rounded-full absolute top-[75rem] -translate-x-[3rem] z-0"></div>
-      {/* <div className="w-[650px] h-[650px] bg-[#e2ecfc] rounded-full absolute top-[100rem] translate-x-[85rem] z-0"></div> */}
-      <div className="grid px-4 py-6 md:gap-8 md:px-6 justify-center w-[100%]">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-          {projects.map((project) => (
-            <ProjectCard key={project.id} project={project} />
-          ))}
+      <div className="container grid w-[100%]  relative">
+        <div className="w-[1000px] h-[1000px] bg-[#e2ecfc] rounded-full absolute top-[5rem] -right-[25rem] z-0"></div>
+        <div className="w-[600px] h-[600px] bg-[#FFF7E3] rounded-full absolute top-[70rem] -translate-x-[15rem] z-0"></div>
+        {/* <div className="w-[200px] h-[200px] bg-[#FFF7E3] rounded-full absolute top-[75rem] -translate-x-[3rem] z-0"></div> */}
+        <div className="grid px-4 py-6 md:gap-8 md:px-6 justify-center w-[100%]">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+            {projects.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+          </div>
+          {isLoading ? <Loading /> : null}
         </div>
-        {isLoading ? <Loading /> : null}
       </div>
     </main>
   );
