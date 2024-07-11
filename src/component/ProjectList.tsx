6375e5cb4ccbfe1519b55b15123886de3ed8cf72
hyojoo <jhj080802@naver.com>
2024-07-11 19:41:45 +0900

    change bg round patern

--- src/component/ProjectList.tsx
+++ src/component/ProjectList.tsx
@@ -34,10 +34,11 @@ export default function ProjectList() {
       className="flex flex-1 overflow-y-auto max-h-[70vh] relative overflow-hidden"
       ref={scrollRef}
     >
-      <div className="w-[350px] h-[350px] bg-[#DAE9FF] rounded-full absolute top-[10rem] translate-x-[45rem] z-0"></div>
-      <div className="w-[400px] h-[400px] bg-[#DAE9FF] rounded-full absolute top-[50rem] -translate-x-[17rem] z-0"></div>
-      {/* <div className="w-[200px] h-[200px] bg-[#DAE9FF] rounded-full absolute top-[95rem] translate-x-[67rem] z-10"></div> */}
-      <div className="w-[650px] h-[650px] bg-[#e2ecfc] rounded-full absolute top-[90rem] translate-x-[85rem] z-0"></div>
+      {/* <div className="w-[650px] h-[650px] bg-[#DAE9FF] rounded-full absolute top-[5rem] translate-x-[65rem] z-0"></div> */}
+      <div className="w-[650px] h-[650px] bg-[#e2ecfc] rounded-full absolute top-[5rem] translate-x-[85rem] z-0"></div>
+      <div className="w-[400px] h-[400px] bg-[#DAE9FF] rounded-full absolute top-[60rem] -translate-x-[17rem] z-0"></div>
+      <div className="w-[200px] h-[200px] bg-[#FFF7E3] rounded-full absolute top-[75rem] -translate-x-[3rem] z-0"></div>
+      {/* <div className="w-[650px] h-[650px] bg-[#e2ecfc] rounded-full absolute top-[100rem] translate-x-[85rem] z-0"></div> */}
       <div className="grid px-4 py-6 md:gap-8 md:px-6 justify-center w-[100%]">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
           {projects.map((project) => (
