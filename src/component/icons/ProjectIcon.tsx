10806cd782ba9cd03dd4f4a908b065eaf2cb4d33
hyojoo <jhj080802@naver.com>
2024-07-11 19:42:15 +0900

    reduce icon size

--- src/component/icons/ProjectIcon.tsx
+++ src/component/icons/ProjectIcon.tsx
@@ -1,8 +1,8 @@
 export default function ProjectIcon(props: { color?: string }) {
   return (
     <svg
-      width="100"
-      height="100"
+      width="50"
+      height="50"
       viewBox="0 0 100 100"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
