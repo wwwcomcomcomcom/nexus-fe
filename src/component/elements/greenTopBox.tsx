c51c062e45b62c839da93c56da238b29d9b6ccdd
hyojoo <jhj080802@naver.com>
2024-07-12 10:13:50 +0900

    incomplete

--- src/component/elements/greenTopBox.tsx
+++ src/component/elements/greenTopBox.tsx
@@ -1,8 +1,8 @@
 export default function GreenTopBox(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="1015"
-      height="606"
+      width="700"
+      height="450"
       viewBox="0 0 1015 606"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
