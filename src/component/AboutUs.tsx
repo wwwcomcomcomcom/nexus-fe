b7144edc6db82e98dc976a54fdd5613ed9cc8dc0
hyojoo <jhj080802@naver.com>
2024-07-12 13:42:32 +0900

    increase name pd, element gap

--- src/component/AboutUs.tsx
+++ src/component/AboutUs.tsx
@@ -5,33 +5,33 @@ export default function AboutUs() {
         <h1 className="font-bold text-2xl">About us</h1>
         <p className="text-[#757575] my-1">NEXUS를 만든 사람들이에요</p>
       </div>
-      <div className="grid grid-cols-3 py-10 ">
+      <div className="grid grid-cols-3 py-10 gap-6">
         <span className="flex flex-col items-center">
-          <h2 className="font-semibold">김담율</h2>
+          <h2 className="font-semibold pb-6">김담율</h2>
           <h3>APP</h3>
           <h3>BackEnd</h3>
         </span>
         <span className="flex flex-col items-center">
-          <h2 className="font-semibold">이서희</h2>
+          <h2 className="font-semibold pb-6">이서희</h2>
           <h3>BackEnd</h3>
         </span>
         <span className="flex flex-col items-center">
-          <h2 className="font-semibold">이세민</h2>
+          <h2 className="font-semibold pb-6">이세민</h2>
           <h3>FrontEnd</h3>
-          <h3>APP</h3>
           <h3>BackEnd</h3>
-        </span>
-        <span className="flex flex-col items-center">
-          <h2 className="font-semibold">이영서</h2>
           <h3>APP</h3>
         </span>
         <span className="flex flex-col items-center">
-          <h2 className="font-semibold">정효주</h2>
+          <h2 className="font-semibold pb-6">이영서</h2>
+          <h3>APP</h3>
+        </span>
+        <span className="flex flex-col items-center">
+          <h2 className="font-semibold pb-6">정효주</h2>
           <h3>FrontEnd</h3>
           <h3>Design</h3>
         </span>
         <span className="flex flex-col items-center">
-          <h2 className="font-semibold">송재욱</h2>
+          <h2 className="font-semibold pb-6">송재욱</h2>
           <h3>FrontEnd</h3>
           <h3>Design</h3>
           <h3>AI</h3>
