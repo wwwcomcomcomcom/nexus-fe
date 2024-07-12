de707efe20f46aa2268dcaf47b400af85e67073b
hyojoo <jhj080802@naver.com>
2024-07-12 10:59:40 +0900

    add kotlin badge

---
+++ src/component/badge/KotlinBadge.tsx
@@ -0,0 +1,96 @@
+export default function KotlinBadge(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="165"
+      height="96"
+      viewBox="0 0 165 96"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g filter="url(#filter0_dd_910_10)">
+        <rect x="28" y="12" width="109" height="40" rx="20" fill="white" />
+        <rect
+          x="28"
+          y="12"
+          width="109"
+          height="40"
+          rx="20"
+          fill="#7F52FF"
+          fill-opacity="0.15"
+        />
+        <path
+          d="M77.112 38H75.112L71.368 32.736L70.248 33.664V38H68.52V26.576H70.248V32.096C70.5253 31.7653 70.8027 31.44 71.08 31.12C71.3573 30.8 71.6347 30.48 71.912 30.16L75 26.576H76.984L72.616 31.568L77.112 38ZM88.3969 32.272C88.3969 33.1573 88.2849 33.9627 88.0609 34.688C87.8475 35.4027 87.5169 36.0213 87.0689 36.544C86.6315 37.0667 86.0769 37.4667 85.4049 37.744C84.7435 38.0213 83.9702 38.16 83.0849 38.16C82.1782 38.16 81.3889 38.0213 80.7169 37.744C80.0555 37.456 79.5009 37.056 79.0529 36.544C78.6155 36.0213 78.2902 35.3973 78.0769 34.672C77.8635 33.9467 77.7569 33.1413 77.7569 32.256C77.7569 31.0827 77.9489 30.0587 78.3329 29.184C78.7169 28.3093 79.3035 27.6267 80.0929 27.136C80.8929 26.6453 81.8955 26.4 83.1009 26.4C84.2742 26.4 85.2502 26.6453 86.0289 27.136C86.8182 27.616 87.4102 28.2987 87.8049 29.184C88.1995 30.0587 88.3969 31.088 88.3969 32.272ZM79.5809 32.272C79.5809 33.1893 79.7035 33.9787 79.9489 34.64C80.1942 35.2907 80.5782 35.7973 81.1009 36.16C81.6235 36.512 82.2849 36.688 83.0849 36.688C83.8955 36.688 84.5569 36.512 85.0689 36.16C85.5809 35.7973 85.9595 35.2907 86.2049 34.64C86.4502 33.9787 86.5729 33.1893 86.5729 32.272C86.5729 30.896 86.2955 29.824 85.7409 29.056C85.1969 28.2773 84.3169 27.888 83.1009 27.888C82.2902 27.888 81.6235 28.064 81.1009 28.416C80.5782 28.768 80.1942 29.2693 79.9489 29.92C79.7035 30.5707 79.5809 31.3547 79.5809 32.272ZM94.235 38H92.507V28.064H89.083V26.576H97.659V28.064H94.235V38ZM99.4106 38V26.576H101.139V36.512H106.019V38H99.4106ZM111.509 38H107.077V37.008L108.437 36.608V27.984L107.077 27.568V26.576H111.509V27.568L110.165 27.984V36.608L111.509 37.008V38ZM123.021 38H120.925L115.149 28.768H115.085C115.096 28.9813 115.106 29.2107 115.117 29.456C115.138 29.7013 115.154 29.9627 115.165 30.24C115.176 30.5067 115.186 30.784 115.197 31.072C115.208 31.3493 115.213 31.632 115.213 31.92V38H113.645V26.576H115.725L121.485 35.76H121.533C121.522 35.6 121.512 35.4027 121.501 35.168C121.49 34.9227 121.48 34.6613 121.469 34.384C121.469 34.1067 121.464 33.824 121.453 33.536C121.442 33.248 121.432 32.976 121.421 32.72V26.576H123.021V38Z"
+          fill="#7F52FF"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_dd_910_10"
+          x="0"
+          y="0"
+          width="165"
+          height="96"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feMorphology
+            radius="4"
+            operator="erode"
+            in="SourceAlpha"
+            result="effect1_dropShadow_910_10"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.0470588 0 0 0 0 0.0470588 0 0 0 0 0.0509804 0 0 0 0.05 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_910_10"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feMorphology
+            radius="4"
+            operator="erode"
+            in="SourceAlpha"
+            result="effect2_dropShadow_910_10"
+          />
+          <feOffset dy="16" />
+          <feGaussianBlur stdDeviation="16" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.0470588 0 0 0 0 0.0470588 0 0 0 0 0.0509804 0 0 0 0.1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect1_dropShadow_910_10"
+            result="effect2_dropShadow_910_10"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect2_dropShadow_910_10"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
