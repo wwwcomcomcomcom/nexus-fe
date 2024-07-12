7e86e6fd2355993f1c680ab37ded13409941a8e2
hyojoo <jhj080802@naver.com>
2024-07-12 11:33:28 +0900

    add text(name, major)

--- src/component/AboutUs.tsx
+++ src/component/AboutUs.tsx
@@ -1,8 +1,42 @@
 export default function AboutUs() {
   return (
-    <div className="w-[100%] px-[300px] py-[100px]">
-      <h1 className="font-bold text-2xl">About us</h1>
-      <p className="text-[#757575] my-1">NEXUS를 만든 사람들이에요</p>
+    <div className="w-[100%] px-[250px] py-[100px]">
+      <div className="pl-20">
+        <h1 className="font-bold text-2xl">About us</h1>
+        <p className="text-[#757575] my-1">NEXUS를 만든 사람들이에요</p>
+      </div>
+      <div className="grid grid-cols-3 py-10 ">
+        <span className="flex flex-col items-center">
+          <h2>김담율</h2>
+          <h3>APP</h3>
+          <h3>BackEnd</h3>
+        </span>
+        <span className="flex flex-col items-center">
+          <h2>이서희</h2>
+          <h3>BackEnd</h3>
+        </span>
+        <span className="flex flex-col items-center">
+          <h2>이세민</h2>
+          <h3>FrontEnd</h3>
+          <h3>APP</h3>
+          <h3>BackEnd</h3>
+        </span>
+        <span className="flex flex-col items-center">
+          <h2>이영서</h2>
+          <h3>APP</h3>
+        </span>
+        <span className="flex flex-col items-center">
+          <h2>정효주</h2>
+          <h3>FrontEnd</h3>
+          <h3>Design</h3>
+        </span>
+        <span className="flex flex-col items-center">
+          <h2>송재욱</h2>
+          <h3>FrontEnd</h3>
+          <h3>Design</h3>
+          <h3>AI</h3>
+        </span>
+      </div>
     </div>
   );
 }
