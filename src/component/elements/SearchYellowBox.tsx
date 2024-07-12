cc84e51466d8fbc68bc7fc327d384032a02e6b18
hyojoo <jhj080802@naver.com>
2024-07-12 14:24:24 +0900

    search yellow box

---
+++ src/component/elements/SearchYellowBox.tsx
@@ -0,0 +1,17 @@
+export default function SearchYellowBox(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="209"
+      height="189"
+      viewBox="0 0 209 189"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M208.344 0.881134L208.344 189L100 189C44.7715 189 -2.86244e-05 144.228 -2.93538e-05 89L-3.05176e-05 0.881136L208.344 0.881134Z"
+        fill="#FFF5DB"
+      />
+    </svg>
+  );
+}
