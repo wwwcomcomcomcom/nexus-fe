c255f514474e04534747c70bd4f3672c9e140da7
hyojoo <jhj080802@naver.com>
2024-07-12 11:36:10 +0900

    change name style

--- src/component/AboutUs.tsx
+++ src/component/AboutUs.tsx
@@ -7,31 +7,31 @@ export default function AboutUs() {
       </div>
       <div className="grid grid-cols-3 py-10 ">
         <span className="flex flex-col items-center">
-          <h2>김담율</h2>
+          <h2 className="font-semibold">김담율</h2>
           <h3>APP</h3>
           <h3>BackEnd</h3>
         </span>
         <span className="flex flex-col items-center">
-          <h2>이서희</h2>
+          <h2 className="font-semibold">이서희</h2>
           <h3>BackEnd</h3>
         </span>
         <span className="flex flex-col items-center">
-          <h2>이세민</h2>
+          <h2 className="font-semibold">이세민</h2>
           <h3>FrontEnd</h3>
           <h3>APP</h3>
           <h3>BackEnd</h3>
         </span>
         <span className="flex flex-col items-center">
-          <h2>이영서</h2>
+          <h2 className="font-semibold">이영서</h2>
           <h3>APP</h3>
         </span>
         <span className="flex flex-col items-center">
-          <h2>정효주</h2>
+          <h2 className="font-semibold">정효주</h2>
           <h3>FrontEnd</h3>
           <h3>Design</h3>
         </span>
         <span className="flex flex-col items-center">
-          <h2>송재욱</h2>
+          <h2 className="font-semibold">송재욱</h2>
           <h3>FrontEnd</h3>
           <h3>Design</h3>
           <h3>AI</h3>
