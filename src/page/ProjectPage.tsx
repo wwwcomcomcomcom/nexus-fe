dea3201a5b5dfef27a9b222ea46d0879678d283a
hyojoo <jhj080802@naver.com>
2024-07-14 23:22:16 +0900

    몰라 내일 할래

--- src/page/ProjectPage.tsx
+++ src/page/ProjectPage.tsx
@@ -1,4 +1,3 @@
-// import { DivProps } from "../../utils/typedef.ts";
 import { useParams } from "react-router-dom";
 import GreenTopBox from "../component/elements/greenTopBox";
 
@@ -14,14 +13,21 @@ function ProjectPage(/*props: DivProps*/) {
         <GreenTopBox
           width={350}
           height="auto"
-          className="translate-y-[250px] translate-x-[350px] z-0"
+          className="absolute top-[-50px] left-[50px] z-10"
         />
-        <h1 className="inline-block justify-center text-5xl font-extrabold bg-white translate-x-[300px] p-4 px-6 rounded-full border-[#F1F1F1] border-[1px] shadow-lg z-20">
-          <p className="">NEXUS</p>
-        </h1>
+        <div className="absolute top-[100px] left-[150px] z-20 flex items-center space-x-4">
+          <h1 className="inline-block text-5xl font-extrabold bg-white p-4 px-6 rounded-full border-[#F1F1F1] border-[1px] shadow-lg">
+            NEXUS
+          </h1>
+          <span className="bg-green-500 text-white py-2 px-4 rounded-full">
+            진행중
+          </span>
+        </div>
 
         <section className="block bg-[#F4F9FF] rounded-l-3xl ml-[35%] align-start p-8 max-h-[70vh] overflow-y-auto z-20">
-          <h2 className="text-xs pb-7 pt-1 text-[#565656]">프로젝트 내용</h2>
+          <h2 className="text-xs pb-7 pt-1 text-[#565656] z-20">
+            프로젝트 내용
+          </h2>
           <p className="">
             예시로 쓰는 프로젝트 소개입니다. 유아이유엑스 처음 해보는데 정말
             어렵네요. 키보드를 일단 치고 있으면 뭔가 일 하는 것처럼 보여서 쓰고
