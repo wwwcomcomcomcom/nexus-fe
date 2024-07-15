0ef82f89ba96fd5a7177eed69caf8c4070689bb8
hyojoo <jhj080802@naver.com>
2024-07-15 11:53:12 +0900

    reduce profle size

--- src/component/AboutUs.tsx
+++ src/component/AboutUs.tsx
@@ -1,76 +1,76 @@
 export default function AboutUs() {
   return (
-    <div className="w-full flex flex-col items-center">
-      <div className="w-full pl-20">
+    <div className="w-full flex flex-col items-center mt-16">
+      <div className="w-5/6 pl-10">
         <h1 className="font-bold text-2xl">About us</h1>
         <p className="text-[#757575] my-1">NEXUS를 만든 사람들이에요</p>
       </div>
-      <div className="grid grid-cols-3 py-10 gap-6 w-full max-w-7xl">
+      <div className="grid grid-cols-3 py-10 gap-6  max-w-7xl w-5/6">
         <div className="flex flex-col items-center">
           <img
             src="https://avatars.githubusercontent.com/u/162972723?v=4"
-            className="rounded-full w-48"
+            className="rounded-full w-24 cursor-pointer"
           ></img>
 
           <h2 className="font-semibold pb-6">김담율</h2>
           <div className="flex flex-col gap-4 w-1/4 min-w-20">
-            <Label text="APP" className="bg-sky-400"></Label>
+            <Label text="APP" className="bg-[#F4F9FF] "></Label>
             {/* <Label text="Backend" className="bg-orange-400"></Label> */}
           </div>
         </div>
         <div className="flex flex-col items-center">
           <img
             src="https://avatars.githubusercontent.com/u/156983141?v=4"
-            className="rounded-full w-48"
+            className="rounded-full w-24 cursor-pointer"
           ></img>
           <h2 className="font-semibold pb-6">이서희</h2>
           <div className="flex flex-col gap-4 w-1/4 min-w-20">
-            <Label text="Backend" className="bg-orange-400"></Label>
+            <Label text="Backend" className="bg-[#FFEDE3]"></Label>
           </div>
         </div>
         <div className="flex flex-col items-center">
           <img
             src="https://avatars.githubusercontent.com/u/68013923?v=4"
-            className="rounded-full w-48"
+            className="rounded-full w-24 cursor-pointer"
           ></img>
           <h2 className="font-semibold pb-6">이세민</h2>
-          <div className="flex flex-col gap-4 w-1/4 min-w-20">
-            <Label text="FrontEnd" className="bg-yellow-400"></Label>
-            <Label text="Backend" className="bg-orange-400"></Label>
-            <Label text="APP" className="bg-sky-400"></Label>
+          <div className="flex flex-col gap-2 w-1/4 min-w-20">
+            <Label text="FrontEnd" className="bg-[#FFF5DB]"></Label>
+            <Label text="Backend" className="bg-[#FFEDE3]"></Label>
+            <Label text="APP" className="bg-[#F4F9FF]"></Label>
           </div>
         </div>
         <div className="flex flex-col items-center">
           <img
             src="https://avatars.githubusercontent.com/u/162972645?v=4"
-            className="rounded-full w-48"
+            className="rounded-full w-24 cursor-pointer"
           ></img>
           <h2 className="font-semibold pb-6">이영서</h2>
           <div className="flex flex-col gap-4 w-1/4 min-w-20">
-            <Label text="APP" className="bg-sky-400"></Label>
+            <Label text="APP" className="bg-[#F4F9FF]"></Label>
           </div>
         </div>
         <div className="flex flex-col items-center">
           <img
             src="https://avatars.githubusercontent.com/u/164720957?v=4"
-            className="rounded-full w-48"
+            className="rounded-full w-24 cursor-pointer"
           ></img>
           <h2 className="font-semibold pb-6">정효주</h2>
-          <div className="flex flex-col gap-4 w-1/4 min-w-20">
-            <Label text="Frontend" className="bg-yellow-400"></Label>
-            <Label text="Design" className="bg-emerald-400"></Label>
+          <div className="flex flex-col gap-2 w-1/4 min-w-20">
+            <Label text="Frontend" className="bg-[#FFF5DB]"></Label>
+            <Label text="Design" className="bg-[#F5E3FF]"></Label>
           </div>
         </div>
         <div className="flex flex-col items-center">
           <img
             src="https://avatars.githubusercontent.com/u/123460320?v=4"
-            className="rounded-full w-48"
+            className="rounded-full w-24 cursor-pointer"
           ></img>
           <h2 className="font-semibold pb-6">송재욱</h2>
-          <div className="flex flex-col gap-4 w-1/4 min-w-20">
-            <Label text="Frontend" className="bg-yellow-400"></Label>
-            <Label text="Design" className="bg-emerald-400"></Label>
-            <Label text="AI" className="bg-purple-400"></Label>
+          <div className="flex flex-col gap-2 w-1/4 min-w-20">
+            <Label text="Frontend" className="bg-[#FFF5DB]"></Label>
+            <Label text="Design" className="bg-[#F5E3FF]"></Label>
+            <Label text="AI" className="bg-[#DEFFEE]"></Label>
           </div>
         </div>
       </div>
