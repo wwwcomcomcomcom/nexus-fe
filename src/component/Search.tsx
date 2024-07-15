5ee7a8d055aeda36c9721b5fb45e643626a5564a
hyojoo <jhj080802@naver.com>
2024-07-15 14:44:37 +0900

    increase pr for center location

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -5,7 +5,7 @@ import SearchYellowBox from "./elements/SearchYellowBox";
 
 export default function Search() {
   return (
-    <div className="flex justify-center mb-80 ">
+    <div className="flex justify-center mb-80 pr-48">
       <div className="absolute w-1/2 h-[200px] flex items-center justify-center -translate-x-1/2 translate-y-1/2 -z-10">
         <SearchYellowBox className="translate-x-10 -translate-y-10" />
       </div>
@@ -21,7 +21,11 @@ export default function Search() {
           className="p-3 h-16 w-[60%] translate-x-[40%] translate-y-[40%] border border-[#F1F1F1] rounded-full shadow-lg bg-white flex items-end"
           onClick={() => document.getElementById("search")?.focus()}
         >
-          <input type="text" id="search" className="w-full h-full outline-none z-20" />
+          <input
+            type="text"
+            id="search"
+            className="w-full h-full outline-none z-20"
+          />
           <SearchBtn className="z-20 cursor-pointer" />
         </div>
       </div>
