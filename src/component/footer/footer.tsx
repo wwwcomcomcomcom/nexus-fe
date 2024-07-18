1361b3de4c4525de1be6a6c6f89988a91fdc9527
hyojoo <jhj080802@naver.com>
2024-07-18 13:34:39 +0900

    reduce border width

--- src/component/footer/footer.tsx
+++ src/component/footer/footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center w-full p-4 border-t border-gray-300">
-      <p className="text-sm text-center text-gray-500 dark:text-gray-400">© 2024 어벤져스, All rights reserved.</p>
+    <footer className="flex items-center justify-center w-full p-4 px-10">
+      <div className="border-t border-[#E4E4E4] w-full">
+        <p className="text-sm text-center text-gray-500 dark:text-gray-400 pt-3">
+          © 2024 어벤져스, All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
