17460bbec350c596f953678320ed57ef5dda6ebc
hyojoo <jhj080802@naver.com>
2024-07-18 20:54:38 +0900

    change from upgrade svg

--- src/component/elements/greenTopBox.tsx
+++ src/component/elements/greenTopBox.tsx
@@ -1,26 +1,26 @@
 export default function GreenTopBox(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="700"
-      height="450"
-      viewBox="0 0 1015 606"
+      width="690"
+      height="514"
+      viewBox="0 0 690 514"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g filter="url(#filter0_dd_680_634)">
+      <g filter="url(#filter0_dd_744_62)">
         <path
-          d="M745.172 509.168C722.858 542.203 685.596 562 645.732 562L103 562C61.5786 562 28 528.421 28 487V87C28 45.5786 61.5786 12 103 12L886.837 12C965.536 12 1013.39 98.701 971.453 165.293L888.743 296.615L745.172 509.168Z"
+          d="M517.829 407.221C496.842 445.907 456.362 470 412.35 470H103C61.5786 470 27.9999 436.421 27.9999 395V87C27.9999 45.5787 61.5786 12 103 12L560.894 12C635.436 12 683.774 90.6209 650.128 157.137L603.657 249.007L517.829 407.221Z"
           fill="#DEFFEE"
         />
       </g>
       <defs>
         <filter
-          id="filter0_dd_680_634"
+          id="filter0_dd_744_62"
           x="0"
           y="0"
-          width="1015"
-          height="606"
+          width="689.025"
+          height="514"
           filterUnits="userSpaceOnUse"
           color-interpolation-filters="sRGB"
         >
@@ -35,7 +35,7 @@ export default function GreenTopBox(props: React.SVGProps<SVGSVGElement>) {
             radius="4"
             operator="erode"
             in="SourceAlpha"
-            result="effect1_dropShadow_680_634"
+            result="effect1_dropShadow_744_62"
           />
           <feOffset dy="4" />
           <feGaussianBlur stdDeviation="2" />
@@ -47,7 +47,7 @@ export default function GreenTopBox(props: React.SVGProps<SVGSVGElement>) {
           <feBlend
             mode="normal"
             in2="BackgroundImageFix"
-            result="effect1_dropShadow_680_634"
+            result="effect1_dropShadow_744_62"
           />
           <feColorMatrix
             in="SourceAlpha"
@@ -59,7 +59,7 @@ export default function GreenTopBox(props: React.SVGProps<SVGSVGElement>) {
             radius="4"
             operator="erode"
             in="SourceAlpha"
-            result="effect2_dropShadow_680_634"
+            result="effect2_dropShadow_744_62"
           />
           <feOffset dy="16" />
           <feGaussianBlur stdDeviation="16" />
@@ -70,13 +70,13 @@ export default function GreenTopBox(props: React.SVGProps<SVGSVGElement>) {
           />
           <feBlend
             mode="normal"
-            in2="effect1_dropShadow_680_634"
-            result="effect2_dropShadow_680_634"
+            in2="effect1_dropShadow_744_62"
+            result="effect2_dropShadow_744_62"
           />
           <feBlend
             mode="normal"
             in="SourceGraphic"
-            in2="effect2_dropShadow_680_634"
+            in2="effect2_dropShadow_744_62"
             result="shape"
           />
         </filter>
