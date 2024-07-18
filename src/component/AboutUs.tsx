7237c2856759d6caadfe3fa32a432ad3f8a8184a
hyojoo <jhj080802@naver.com>
2024-07-18 12:06:54 +0900

    add github icon

--- src/component/AboutUs.tsx
+++ src/component/AboutUs.tsx
@@ -21,7 +21,24 @@ export default function AboutUs() {
   return (
     <div className="w-full flex flex-col items-center mt-16">
       <div className="w-3/4 pl-10">
-        <h1 className="font-bold text-2xl">About us</h1>
+        <h1 className="font-bold text-2xl">
+          About us
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="ml-2 h-5 w-5 inline mb-1"
+          >
+            <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
+            <path d="M9 18c-4.51 2-5-2-7-2"></path>
+          </svg>
+        </h1>
         <p className="text-[#757575] my-1">NEXUS를 만든 사람들이에요</p>
       </div>
       <div className="grid grid-cols-3 py-8 gap-6  max-w-7xl w-5/6">
