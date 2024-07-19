f6e1cc4ffcee55e7d13400aa986592e8b37e9fcc
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-19 09:29:24 +0900

    todo: responsive

--- src/page/ProjectPage.tsx
+++ src/page/ProjectPage.tsx
@@ -10,9 +10,9 @@ function ProjectPage(/*props: DivProps*/) {
   return (
     <main className="flex flex-col pt-20">
       <div className="w-full h-[70vh] flex justify-end">
-        <div className="w-2/3 h-full bg-[#F4F9FF] rounded-3xl relative">
-          <div className="absolute w-fit h-fit flex justify-start items-start -translate-x-10 -translate-y-10">
-            <GreenTopBox className="w-[30rem] h-fit" />
+        <div className="md:w-2/3 w-full h-full bg-[#F4F9FF] rounded-3xl relative">
+          <div className="absolute w-full h-fit flex justify-start items-start -translate-x-10 -translate-y-10">
+            <GreenTopBox className="w-2/3 h-fit" />
             <div className="absolute bg-white rounded-full text-6xl font-extrabold py-6 px-14 translate-y-3/4 -translate-x-[80%] shadow-xl">
               NEXUS
               <div className="absolute bg-white shadow-xl flex items-center gap-2 rounded-full p-2 pr-4 border border-gray-200 translate-x-[115%]">
