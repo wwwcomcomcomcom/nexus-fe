043cb0942a806143126b7828198a21063ef1f6ea
hyojoo <jhj080802@naver.com>
2024-07-22 17:39:22 +0900

    change left arrow

--- src/component/elements/LeftArrowIcon.tsx
+++ src/component/elements/LeftArrowIcon.tsx
@@ -1,14 +1,18 @@
 export default function LeftArrowIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="21"
-      height="34"
-      viewBox="0 0 21 34"
+      width="12"
+      height="22"
+      viewBox="0 0 12 22"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M20 32L2.48062 17L20 2" stroke="black" stroke-width="3" />
+      <path
+        d="M11 21L1.70711 11.7071C1.31658 11.3166 1.31658 10.6834 1.70711 10.2929L11 1.00001"
+        stroke="black"
+        stroke-linecap="round"
+      />
     </svg>
   );
 }
