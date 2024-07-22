8f890b148f15391f559dbec98049da04f44ed848
hyojoo <jhj080802@naver.com>
2024-07-22 12:47:26 +0900

    left arrow icon

---
+++ src/component/elements/LeftArrowIcon.tsx
@@ -0,0 +1,14 @@
+export default function LeftArrowIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="21"
+      height="34"
+      viewBox="0 0 21 34"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M20 32L2.48062 17L20 2" stroke="black" stroke-width="3" />
+    </svg>
+  );
+}
