9b0ecdcc88ab5240506211f8618df2c2003c50f2
hyojoo <jhj080802@naver.com>
2024-07-22 20:02:13 +0900

    unmodified under green box size

--- src/page/PostPage.tsx
+++ src/page/PostPage.tsx
@@ -23,7 +23,7 @@ function PostPage() {
       </div>
       <div className="mx-[10%] items-center relative ">
         <div className="relative">
-          <div className="flex-col absolute flex z-50 ">
+          <div className="flex-col absolute flex z-40 ">
             <GreenTopBox />
           </div>
           <div className=" flex justify-center rounded-[40px] shadow-xl bg-white border border-[#F2F2F2] z-0 px-16 w-[80%] ml-36 pt-[29rem] pb-[6rem] translate-y-16">
@@ -46,11 +46,10 @@ function PostPage() {
               게시글 게시글임시로 적는 게시글 게시글임시로 적는 게시글
               게시글임시로 적는 게시글
             </p>
+            <div className="absolute flex flex-col z-40 translate-x-[40%] translate-y-[10rem] ">
+              <PostUnderGreenBox />
+            </div>
           </div>
-        </div>
-
-        <div className="absolute flex flex-col z-50 translate-y-[200%] ">
-          <PostUnderGreenBox />
         </div>
       </div>
     </main>
