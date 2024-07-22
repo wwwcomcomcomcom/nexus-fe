25f643cf865098793a43906d561a32dacf5d445b
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-22 23:26:27 +0900

    svg without shadow. add shadow with css

--- src/component/elements/GreenBottomBox.tsx
+++ src/component/elements/GreenBottomBox.tsx
@@ -1,74 +1,20 @@
 export default function GreenBottomBox(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="655"
-      height="284"
-      viewBox="0 0 655 284"
+      width="591"
+      height="220"
+      viewBox="0 0 591 220"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      style={{
+        filter: "drop-shadow(0px 8px 8px rgba(0, 0, 0, 0.25))",
+      }}
       {...props}
     >
-      <g filter="url(#filter0_dd_1176_955)">
-        <path
-          d="M40.7385 126C14.4911 176.005 50.7576 236 107.233 236L548 236C589.421 236 623 202.421 623 161V91C623 49.5786 589.421 16 548 16L158.926 16C121.758 16 87.6568 36.614 70.3826 69.5238L40.7385 126Z"
-          fill="#DEFFEE"
-        />
-      </g>
-      <defs>
-        <filter
-          id="filter0_dd_1176_955"
-          x="0.032959"
-          y="0"
-          width="654.967"
-          height="284"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
-        >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dy="4" />
-          <feGaussianBlur stdDeviation="2" />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0.0470588 0 0 0 0 0.0470588 0 0 0 0 0.0509804 0 0 0 0.05 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_1176_955"
-          />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dy="16" />
-          <feGaussianBlur stdDeviation="16" />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0.0470588 0 0 0 0 0.0470588 0 0 0 0 0.0509804 0 0 0 0.1 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="effect1_dropShadow_1176_955"
-            result="effect2_dropShadow_1176_955"
-          />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect2_dropShadow_1176_955"
-            result="shape"
-          />
-        </filter>
-      </defs>
+      <path
+        d="M8.73849 110C-17.5089 160.005 18.7576 220 75.2326 220L516 220C557.421 220 591 186.421 591 145V75C591 33.5786 557.421 0 516 0L126.926 0C89.7583 0 55.6568 20.614 38.3826 53.5238L8.73849 110Z"
+        fill="#DEFFEE"
+      />
     </svg>
   );
 }
