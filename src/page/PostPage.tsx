85089e1e2cbe195c943402e69b1dc9bc2de40e2f
hyojoo <jhj080802@naver.com>
2024-07-22 19:57:04 +0900

    텍스트 길이에 따른 도형 배치, 사실 혹시 몰라 하는 커밋

--- src/page/PostPage.tsx
+++ src/page/PostPage.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import GreenTopBox from "../component/elements/greenTopBox";
 import LeftArrowIcon from "../component/elements/LeftArrowIcon";
+import PostUnderGreenBox from "../component/elements/PostUnderGreenBox";
 
 function PostPage() {
   const param = useParams();
@@ -20,25 +21,36 @@ function PostPage() {
       <div className="mx-[5%] py-10 cursor-pointer" onClick={handleClick}>
         <LeftArrowIcon />
       </div>
-      <div className="mx-[10%] items-center">
-        <div className="flex justify-center rounded-[40px] shadow-xl bg-white border border-[#F2F2F2] z-50  px-16 w-[80%] ml-36 pt-[29rem] pb-[6rem] translate-y-16">
-          <p className="">
-            임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-            게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
-            적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-            게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
-            적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-            게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
-            적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-            게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
-            적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-            게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
-            적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-            게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글{" "}
-          </p>
+      <div className="mx-[10%] items-center relative ">
+        <div className="relative">
+          <div className="flex-col absolute flex z-50 ">
+            <GreenTopBox />
+          </div>
+          <div className=" flex justify-center rounded-[40px] shadow-xl bg-white border border-[#F2F2F2] z-0 px-16 w-[80%] ml-36 pt-[29rem] pb-[6rem] translate-y-16">
+            <p className="">
+              임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
+              게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
+              적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
+              게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
+              적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
+              게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
+              적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
+              게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
+              적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
+              게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
+              적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
+              게시글임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글
+              게시글임시로 적는 게시글임시로 적는 게시글 게시글임시로 적는
+              게시글 게시글임시로 적는 게시글 게시글임시로 적는 게시글
+              게시글임시로 적는 게시글 게시글임시로 적는 게시글임시로 적는
+              게시글 게시글임시로 적는 게시글 게시글임시로 적는 게시글
+              게시글임시로 적는 게시글
+            </p>
+          </div>
         </div>
-        <div className="flex z-30 -translate-y-[46rem]">
-          <GreenTopBox />
+
+        <div className="absolute flex flex-col z-50 translate-y-[200%] ">
+          <PostUnderGreenBox />
         </div>
       </div>
     </main>
