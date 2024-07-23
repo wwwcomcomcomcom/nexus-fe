86737292ff2a280bb5c27fff26d4282a563d1e1e
hyojoo <jhj080802@naver.com>
2024-07-23 19:04:13 +0900

    add 뿌슝빠슝, adjust project name and project status locaition

--- src/page/ProjectPage.tsx
+++ src/page/ProjectPage.tsx
@@ -24,7 +24,10 @@ function ProjectPage(/*props: DivProps*/) {
         <div className="md:w-2/3 w-full h-full bg-[#F4F9FF] rounded-3xl relative">
           <div className="absolute w-full h-fit flex justify-start items-start -translate-x-10 -translate-y-10 max-md:translate-x-0">
             <ProjectGreenTopBox className="w-2/3 max-w-[30rem] h-fit max-md:w-1/2" />
-            <div className="absolute bg-white rounded-full text-6xl font-extrabold py-6 px-14 translate-y-3/4 -translate-x-[80%] shadow-xl">
+            <div className="absolute text-[#757575] top-[25%] left-[8%]">
+              NEXUS 안에 NEXUS ?? 뿌슝빠슝
+            </div>
+            <div className="absolute bg-white rounded-full text-6xl font-extrabold py-6 px-14 translate-y-1/2 -translate-x-[80%] shadow-xl">
               NEXUS
               <div className="absolute bg-white shadow-xl flex items-center gap-2 rounded-full p-2 pr-4 border border-gray-200 translate-x-[115%]">
                 <div className="w-10 h-10 rounded-full bg-orange-400"></div>
