bae274873620d559834e9a3c57e57d8ae5d0d70f
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-23 19:19:55 +0900

    remove comment code

--- src/page/PostPage.tsx
+++ src/page/PostPage.tsx
@@ -68,41 +68,6 @@ function PostPage() {
             <PencilIcon className="w-6" />
           </label>
         </div>
-        {/* <div className="grid gap-4 px-4 md:gap-8 md:px-6 w-[40%] z-0 relative ">
-          <div className="flex bg-[#FFF5DB] w-[700px] h-[700px] rounded-full z-0 absolute top-56 -translate-x-64"></div>
-        </div>
-        <div className="mx-[10%] items-center relative ">
-          <div className="relative">
-            <div className="flex-col absolute flex z-40 ">
-              <GreenTopBox />
-            </div>
-            <div className=" flex justify-center rounded-[40px] shadow-xl bg-white border border-[#F2F2F2] z-0 px-16 w-[80%] ml-36 pt-[29rem] pb-[6rem] translate-y-16">
-              <p className="">
-                임시로 적는 게시글임시로 적는 게시글임시로 적는 게시글임시로
-                적는 게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글임시로 적는
-                게시글임시로 적는 게시글임시로 적는 게시글 게시글임시로 적는
-                게시글임시로 적는 게시글 게시글임시로 적는 게시글 게시글임시로
-                적는 게시글 게시글임시로 적는 게시글 게시글임시로 적는 게시글
-                게시글임시로 적는 게시글임시로 적는 게시글 게시글임시로 적는
-                게시글 게시글임시로 적는 게시글 게시글임시로 적는 게시글
-              </p>
-              <div className="absolute flex flex-col z-40 translate-x-[40%] translate-y-[10rem] ">
-                <PostUnderGreenBox />
-              </div>
-            </div>
-          </div>
-        </div> */}
       </main>
     </>
   );
