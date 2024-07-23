eca85b4ae7dc595b92cea636f27482d790ecb220
hyojoo <jhj080802@naver.com>
2024-07-23 20:33:55 +0900

    adjust card size

--- src/component/projectCard/ProfileCard.tsx
+++ src/component/projectCard/ProfileCard.tsx
@@ -7,16 +7,16 @@ interface ProfileCardProps extends DivProps {
 
 export default function ProfileCard(props: ProfileCardProps) {
   return (
-    <div className="flex w-fit h-24 relative rounded-full shadow-lg">
+    <div className="flex w-fit h-16 relative rounded-full shadow-lg">
       <img
-        className="flex rounded-full h-full w-auto"
+        className="flex rounded-full h-full w-auto cursor-pointer"
         onClick={() => (window.location.href = `${props.profile.url}`)}
         src={props.profile.imgUrl}
         alt="profile"
       ></img>
-      <div className="flex justify-between pr-8 pl-4 gap-2 items-center">
-        <div className="flex font-semibold text-2xl">{props.profile.name}</div>
-        <div className="flex text-2xl">{props.profile.role}</div>
+      <div className="flex justify-between pr-5 pl-2 gap-2 items-center">
+        <div className="flex font-semibold ">{props.profile.name}</div>
+        <div className="flex ">{props.profile.role}</div>
       </div>
     </div>
   );
