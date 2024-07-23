d8f2ecaa6f993849542663e0f5fdf15d7ed8418d
hyojoo <jhj080802@naver.com>
2024-07-23 12:30:28 +0900

    adjust post and project location , increase profile photo size

--- src/page/ProfilePage.tsx
+++ src/page/ProfilePage.tsx
@@ -8,7 +8,7 @@ export default function ProfilePage() {
   const { isLogin, user } = useUpdatedUserStore();
   if (isLogin() !== false) return <h1>You are not logged in</h1>;
   return (
-    <div className="flex justify-center pt-16">
+    <div className="flex justify-center pt-20">
       <div className="flex flex-col items-center space-x-4 grow basis-0 bg-[url('profile-bg.svg')]">
         <div className="flex flex-row w-full">
           <div className="grow flex items-center justify-center">
@@ -31,29 +31,27 @@ export default function ProfilePage() {
           </div>
           <div className="grow">
             <img
-              className="w-52 h-52 rounded-full shadow-inner"
+              className="w-64 h-64 rounded-full shadow-inner"
               src={user.profileImageUrl as string}
               alt="profile"
             />
           </div>
         </div>
       </div>
-      <div className="grow basis-0 flex flex-col gap-4 px-14">
-        <div>
-          <h1 className="text-2xl font-bold px-2 pb-3">Projects</h1>
-          <div className="grid grid-cols-2 gap-y-4 h-[34rem] w-3/4 overflow-y-scroll scrollbar-hide mb-4 p-4">
-            {getAllProjectEntity(10).map((project) => (
-              <ProjectCard
-                key={project.id}
-                project={project}
-                className="h-[8rem]"
-              />
-            ))}
-          </div>
+      <div className="grow basis-0 flex flex-col px-14 items-center">
+        <h1 className="text-2xl font-bold px-2 pb-3 ">Projects</h1>
+        <div className="grid grid-cols-2 h-[35rem] w-fit gap-7  overflow-y-scroll scrollbar-hide mb-4 p-4 place-items-center">
+          {getAllProjectEntity(10).map((project) => (
+            <ProjectCard
+              key={project.id}
+              project={project}
+              className="h-[8rem]"
+            />
+          ))}
         </div>
-        <div className="py-14">
+        <div className="py-10 flex flex-col items-center">
           <h1 className="text-2xl font-bold pb-3">Posts</h1>
-          <div className="grid gap-y-4 h-[24rem] overflow-y-scroll scrollbar-hide p-4 w-[80%]">
+          <div className="grid gap-y-4 h-[24.5rem] overflow-y-scroll scrollbar-hide p-4 w-[80%]">
             {getAllPostEntity(10).map((post) => (
               <PostCard key={post.id} post={post} />
             ))}
