3fce6b947525efdab18d4c2438e60f15441545dc
hyojoo <jhj080802@naver.com>
2024-07-25 15:11:14 +0900

    엄청난 뱃지 얻기 성공

--- src/component/projectCard/NeedCard.tsx
+++ src/component/projectCard/NeedCard.tsx
@@ -3,6 +3,12 @@ import { NeedEntity } from "../../entity/NeedEntity";
 import { DivProps } from "../../utils/typedef";
 import JavascriptBadge from "../badge/JavascriptBadge";
 import ReactBadge from "../badge/ReactBadge";
+import FigmaBadge from "../badge/FigmaBadge";
+import FlutterBadge from "../badge/FlutterBadge";
+import JavaBadge from "../badge/JavaBadge";
+import KotlinBadge from "../badge/KotlinBadge";
+import SpringBadge from "../badge/SpringBadge";
+import TypescriptBadge from "../badge/TypescriptBadge";
 
 interface NeedCardProps extends DivProps {
   need: NeedEntity;
@@ -29,11 +35,28 @@ export default function NeedCard(props: NeedCardProps) {
         <div>
           <div className="text-sm">지원 자격</div>
           <div className="flex">
-            {/* {needs.map((need) => (
-              <NeedCard need={need} />
-            ))} */}
-            <JavascriptBadge />
-            <ReactBadge />
+            {props.need.stack.map((stack) => {
+              switch (stack) {
+                case "Figma":
+                  return <FigmaBadge />;
+                case "Flutter":
+                  return <FlutterBadge />;
+                case "Java":
+                  return <JavaBadge />;
+                case "Javascript":
+                  return <JavascriptBadge />;
+                case "Kotlin":
+                  return <KotlinBadge />;
+                case "React":
+                  return <ReactBadge />;
+                case "Spring":
+                  return <SpringBadge />;
+                case "Typescript":
+                  return <TypescriptBadge />;
+                default:
+                  return "ERR";
+              }
+            })}
           </div>
         </div>
       </div>
