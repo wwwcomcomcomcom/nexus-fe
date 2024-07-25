d93205c07cd2fdf82b81b8f1c000dd8f41a06ce3
hyojoo <jhj080802@naver.com>
2024-07-25 09:53:47 +0900

    text-sm

--- src/component/projectCard/NeedCard.tsx
+++ src/component/projectCard/NeedCard.tsx
@@ -7,19 +7,23 @@ interface NeedCardProps extends DivProps {
 
 export default function NeedCard(props: NeedCardProps) {
   return (
-    <div className="bg-white rounded-3xl shadow-md w-fit p-6 pr-20">
-      <div className="grid gap-1">
+    <div className="bg-white rounded-3xl shadow-md w-fit p-5 pr-16">
+      <div className="grid gap-2">
         <div className="grid grid-flow-col justify-start">
-          <div className="flex">모집 분야</div>
-          <div className="flex font-semibold pl-2">{props.need.role}</div>
+          <div className="flex text-sm">모집 분야</div>
+          <div className="flex font-semibold pl-2 text-sm">
+            {props.need.role}
+          </div>
         </div>
         <div className="grid grid-flow-col justify-start">
-          <div className="flex">모집 인원</div>
-          <div className="flex font-semibold pl-2">{props.need.number}인</div>
+          <div className="flex text-sm">모집 인원</div>
+          <div className="flex font-semibold pl-2 text-sm">
+            {props.need.number}인
+          </div>
         </div>
         <div>
-          <div>지원 자격</div>
-          <div>스택 벳지 넣는 곳</div>
+          <div className="text-sm">지원 자격</div>
+          <div className="">스택 벳지 넣는 곳</div>
         </div>
       </div>
     </div>
