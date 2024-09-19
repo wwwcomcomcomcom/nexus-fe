739f6df43039f89a458c4e0e9cb7315aef6e5ae2
hyojoo <jhj080802@naver.com>
2024-09-19 18:11:03 +0900

    feat : git url

--- src/shared/apiMockup.ts
+++ src/shared/apiMockup.ts
@@ -49,7 +49,7 @@ export function generateProfileEntity(words?: string[]): ProfileEntity {
   }
   const profileEntity: ProfileEntity = {
     name: "정효주",
-    url: "https://github.com/jhj080802",
+    url: "https://github.com/h-0y28",
     imgUrl: "https://avatars.githubusercontent.com/u/164720957?v=4",
     role: "FrontEnd",
   };
