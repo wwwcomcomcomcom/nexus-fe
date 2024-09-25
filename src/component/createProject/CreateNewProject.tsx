f16f58fbbd023e9340c271f75bd220114e84721d
hyojoo <jhj080802@naver.com>
2024-09-25 20:08:16 +0900

    left arrow icon

--- src/component/createProject/CreateNewProject.tsx
+++ src/component/createProject/CreateNewProject.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { NewProjectStore, useProjectFormStore } from "./projectFormStore";
+import LeftArrowIcon from "../icons/LeftArrowIcon";
 
 type FormStore = {
   frontend: number;
@@ -40,24 +41,10 @@ export default function CreateNewProject({
   return (
     <>
       <div
-        className="inline-flex items-center justify-center rounded-full bg-gray-100 p-3 m-2 absolute left-0"
+        className="inline-flex items-center justify-center m-2 absolute left-0 p-8 cursor-pointer"
         onClick={() => navigate("/projects")}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="h-5 w-5"
-        >
-          <path d="M12 19L5 12l7-7"></path>
-          <path d="M19 12H5"></path>
-        </svg>
+        <LeftArrowIcon />
       </div>
       <div className="flex flex-col border rounded-xl border-gray-400 p-12 my-10 gap-4 h-fit">
         <h1 className="text-[2rem] font-bold">새 프로젝트 만들기</h1>
