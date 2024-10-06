b387771b97b9a854c29a7eee2e8f1aaf7affd9fd
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-10-06 17:21:06 +0900

    remove unused comment code

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -29,17 +29,3 @@ export function useUpdatedUserStore() {
   }, []);
   return store;
 }
-
-// export function getUserInfo() {
-//   axios
-//     .get(`${ApiBaseUrl}/api/user/info`)
-//     .then((res) => {
-//       if (res.status === 200 && res.data.name) {
-//         return res.data;
-//       }
-//       throw new Error(`Invalid response ${res.status}`);
-//     })
-//     .catch((e) => {
-//       throw new Error("Failed to get user info" + e.message);
-//     });
-// }
