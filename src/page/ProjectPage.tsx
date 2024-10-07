90f5739648d37dffc00ce4f6a24b7b593f55e9d8
hyojoo <jhj080802@naver.com>
2024-10-07 16:53:46 +0900

    Get project title and description from generateProjectEntity

--- src/page/ProjectPage.tsx
+++ src/page/ProjectPage.tsx
@@ -37,7 +37,7 @@ function ProjectPage() {
           <div className="absolute w-full h-fit flex justify-start items-start -translate-x-10 -translate-y-10 max-md:translate-x-0">
             <ProjectGreenTopBox className="w-1/2 max-w-[30rem] h-fit max-md:w-1/2" />
             <div className="absolute bg-white rounded-full text-6xl font-extrabold py-5 px-10 translate-y-1/2 -translate-x-[80%] shadow-xl">
-              NEXUS
+              {project.name}
               <div className="absolute bg-white shadow-xl flex items-center gap-2 rounded-full p-2 pr-4 border border-gray-200 translate-x-[115%]">
                 <div
                   className="w-8 h-8 rounded-full"
@@ -47,7 +47,7 @@ function ProjectPage() {
               </div>
             </div>
             <div className="text-sm absolute text-[#757575] top-[25%] left-[8%]">
-              NEXUS 안에 NEXUS ?? 뿌슝빠슝
+              {project.description}
             </div>
           </div>
 
