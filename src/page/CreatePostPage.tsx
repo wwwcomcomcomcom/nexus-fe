9f288dbb706f43670cf5f63f4c5b857f4348384b
hyojoo <jhj080802@naver.com>
2024-10-14 13:47:49 +0900

    Fix Background Pattern Overflow

--- src/page/CreatePostPage.tsx
+++ src/page/CreatePostPage.tsx
@@ -57,80 +57,82 @@ export default function CreatePostPage() {
           <LeftArrowIcon className="w-3 h-auto" />
         </span>
       </div>
-      <div className="text-center mb-6">
+      <main className="overflow-hidden relative">
         <div className="w-[50vw] h-[50vw] sm:w-[700px] sm:h-[700px] rounded-full absolute bg-[#FFF5DB] left-0 -z-10 -translate-x-1/2"></div>
         <div className="w-[40vw] h-[40vw] sm:w-[600px] sm:h-[600px] rounded-full absolute bg-[#FFF5DB] right-0 -z-10 translate-x-1/3 top-[60rem]"></div>
-        <h1 className="text-2xl font-bold text-gray-800">Create Post</h1>
-        <p className="text-gray-500 text-sm">
-          글을 작성하고 사람들과 공유해보세요.
-        </p>
-      </div>
-      <div className="flex flex-col items-center h-fit p-4 pb-10">
-        {/* 컨테이너 */}
-        <div className="w-full max-w-3xl mt-3 p-14 bg-white rounded-3xl shadow-lg border border-[#F2F2F2]">
-          <div className="flex flex-col gap-6">
-            {/* 프로젝트 이름 */}
-            <div>
-              <label
-                htmlFor="projectName"
-                className="block font-medium text-gray-700"
-              >
-                제목
-              </label>
-              <input
-                id="projectName"
-                type="text"
-                className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-[#0000FF4D] focus:border-[#0000FF4D] text-lg outline-none"
-                value={formStore.name}
-                onInput={(e: React.FormEvent<HTMLInputElement>) =>
-                  formStore.setProjectForm({ name: e.currentTarget.value })
-                }
-              />
-            </div>
+        <div className="text-center mb-6">
+          <h1 className="text-2xl font-bold text-gray-800">Create Post</h1>
+          <p className="text-gray-500 text-sm">
+            글을 작성하고 사람들과 공유해보세요.
+          </p>
+        </div>
+        <div className="flex flex-col items-center h-fit p-4 pb-10">
+          {/* 컨테이너 */}
+          <div className="w-full max-w-3xl mt-3 p-14 bg-white rounded-3xl shadow-lg border border-[#F2F2F2]">
+            <div className="flex flex-col gap-6">
+              {/* 프로젝트 이름 */}
+              <div>
+                <label
+                  htmlFor="projectName"
+                  className="block font-medium text-gray-700"
+                >
+                  제목
+                </label>
+                <input
+                  id="projectName"
+                  type="text"
+                  className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-[#0000FF4D] focus:border-[#0000FF4D] text-lg outline-none"
+                  value={formStore.name}
+                  onInput={(e: React.FormEvent<HTMLInputElement>) =>
+                    formStore.setProjectForm({ name: e.currentTarget.value })
+                  }
+                />
+              </div>
 
-            {/* 프로젝트 설명 */}
-            <div>
-              <label
-                htmlFor="projectDescription"
-                className="block font-medium text-gray-700"
-              >
-                내용
-              </label>
-              <textarea
-                id="projectDescription"
-                className="w-full mt-1 p-2 border border-gray-300 rounded-md text-sm focus:ring-[#0000FF4D] focus:border-[#0000FF4D]"
-                value={formStore.description}
-                rows={1} // 기본 높이를 1행으로 설정
-                style={{
-                  overflow: "hidden",
-                  resize: "none",
-                  minHeight: "100px",
-                  outline: "none",
-                }}
-                onInput={(e: React.FormEvent<HTMLTextAreaElement>) => {
-                  adjustHeight(e as React.ChangeEvent<HTMLTextAreaElement>); // 자동 크기 조정 함수 호출
-                  formStore.setProjectForm({
-                    description: e.currentTarget.value,
-                  });
-                }}
-              />
-            </div>
+              {/* 프로젝트 설명 */}
+              <div>
+                <label
+                  htmlFor="projectDescription"
+                  className="block font-medium text-gray-700"
+                >
+                  내용
+                </label>
+                <textarea
+                  id="projectDescription"
+                  className="w-full mt-1 p-2 border border-gray-300 rounded-md text-sm focus:ring-[#0000FF4D] focus:border-[#0000FF4D]"
+                  value={formStore.description}
+                  rows={1} // 기본 높이를 1행으로 설정
+                  style={{
+                    overflow: "hidden",
+                    resize: "none",
+                    minHeight: "100px",
+                    outline: "none",
+                  }}
+                  onInput={(e: React.FormEvent<HTMLTextAreaElement>) => {
+                    adjustHeight(e as React.ChangeEvent<HTMLTextAreaElement>); // 자동 크기 조정 함수 호출
+                    formStore.setProjectForm({
+                      description: e.currentTarget.value,
+                    });
+                  }}
+                />
+              </div>
 
-            {/* Submit Button */}
-            <button
-              className={`w-full py-2 px-4 rounded-md text-md font-semibold shadow-md ${
-                isDisabled
-                  ? "bg-gray-300 cursor-not-allowed text-gray-700 "
-                  : "bg-gradient-to-r from-blue-500 to-indigo-500 hover:shadow-lg transition transform hover:-translate-y-0.5 active:translate-y-0.5 text-white"
-              }`}
-              disabled={isDisabled}
-              onClick={submitProjectData}
-            >
-              Create Post
-            </button>
+              {/* Submit Button */}
+              <button
+                className={`w-full py-2 px-4 rounded-md text-md font-semibold shadow-md ${
+                  isDisabled
+                    ? "bg-gray-300 cursor-not-allowed text-gray-700 "
+                    : "bg-gradient-to-r from-blue-500 to-indigo-500 hover:shadow-lg transition transform hover:-translate-y-0.5 active:translate-y-0.5 text-white"
+                }`}
+                disabled={isDisabled}
+                onClick={submitProjectData}
+              >
+                Create Post
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
