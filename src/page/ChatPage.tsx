ccc20252819b7f458573f7c5c67049a7d8022c20
hyojoo <jhj080802@naver.com>
2024-10-16 20:33:28 +0900

    주석

--- src/page/ChatPage.tsx
+++ src/page/ChatPage.tsx
@@ -3,11 +3,16 @@ import PencilIcon from "../component/icons/PencilIcon";
 export default function ChatPage() {
   return (
     <div className="flex h-lvh">
+      {/* 채팅자 목록 */}
       <div className="h-full grow max-w-lg min-w-64 flex flex-col p-6 pl-0">
-        <div className="rounded-r-3xl bg-slate-200 h-full"></div>
+        <div className="rounded-r-3xl bg-slate-200 h-full">
+          <p></p>
+        </div>
       </div>
       <div className="h-full grow-[2] flex flex-col py-6 gap-8">
+        {/* 채팅 내용이 담긴 상자 */}
         <div className="h-full rounded-3xl border border-[#FEFEFE] grow shadow-[0_10px_50px_0px_rgb(0,0,0,0.1)]"></div>
+        {/* 채팅 입력 - textarea */}
         <label
           className="bg-white w-full flex items-center border border-[#F2F2F2] rounded-[30px] px-6 py-1 shadow-md"
           htmlFor="commentInput"
