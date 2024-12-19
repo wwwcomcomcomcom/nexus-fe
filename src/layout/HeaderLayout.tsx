38003b29aaac871c4e9c72ae9a9e4f6b4aeed8f9
h-0y28 <s24015@gsm.hs.kr>
2024-12-19 11:14:29 +0900

    add: pt

--- src/layout/HeaderLayout.tsx
+++ src/layout/HeaderLayout.tsx
@@ -1,9 +1,13 @@
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "../component/headBar/Header.tsx";
 
 export default function HeaderLayout() {
-  return <>
-    <Header/>
-    <Outlet/>
-  </>;
+  return (
+    <>
+      <Header />
+      <main className="pt-16">
+        <Outlet />
+      </main>
+    </>
+  );
 }
