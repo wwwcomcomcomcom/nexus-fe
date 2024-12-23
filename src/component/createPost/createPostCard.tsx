755c40fdaae392048cc4eba8a44329d459e8641a
h-0y28 <s24015@gsm.hs.kr>
2024-12-23 17:28:59 +0900

    디자인 변경

--- src/component/createPost/createPostCard.tsx
+++ src/component/createPost/createPostCard.tsx
@@ -16,16 +16,14 @@ export default function CreatePostCard() {
           <p className="mt-4 text-gray-500">새 게시글</p>
         </div>
       ) : (
-        <div className="p-5 pb-10 rounded-2xl border-2 border-dashed border-gray-300 flex flex-col items-center justify-center bg-white">
-          <p className="text-gray-500">
-            게시글을 작성하기 위해서는 로그인이 필요합니다. <br />
-            <span
-              className="text-blue-500 cursor-pointer underline"
-              onClick={() => navigate("/login")}
-            >
-              로그인 페이지로 이동
-            </span>
-          </p>
+        <div
+          className="p-5 pb-10 rounded-2xl border-2 border-dashed border-gray-300 flex flex-col items-center justify-center bg-white hover:scale-105 transition "
+          onClick={() => navigate("/login")}
+        >
+          <div className="text-5xl text-gray-400">+</div>
+          <span className="text-blue-500 cursor-pointer underline">
+            로그인이 필요합니다.
+          </span>
         </div>
       )}
     </div>
